78db0dec970878c428ab96fa874f9b60c3e96c27
Derek Leadbetter <dleadbetter55@gmail.com>
2020-05-12 14:28:58 -0400

    Testing using i18n object directly

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,5 +1,4 @@
 import i18next from 'i18next';
-import { initReactI18next } from 'react-i18next';
 
 import en from './en.json';
 
--- src/semantic-ui/LoginModal.js
+++ src/semantic-ui/LoginModal.js
@@ -11,7 +11,6 @@ import {
   Message,
   Modal
 } from 'semantic-ui-react';
-import { I18nextProvider, withTranslation } from 'react-i18next';
 import i18n from '../i18n/i18n';
 import './LoginModal.css';
 
@@ -23,80 +22,77 @@ type Props = {
   onPasswordChange: () => void,
   onUsernameChange: () => void,
   open: boolean,
-  t: () => string,
   trigger: () => Component
 };
 
 const LoginModal = (props: Props) => (
-  <I18nextProvider i18n={i18n}>
-    <Modal
-      as={Form}
-      error={props.loginFailed}
-      open={props.open}
-      size='small'
-      trigger={props.trigger}
+  <Modal
+    as={Form}
+    error={props.loginFailed}
+    open={props.open}
+    size='small'
+    trigger={props.trigger}
+  >
+    <Header
+      icon='user circle'
+      content={i18n.t('LoginModal.header')}
+    />
+    <Message
+      error
+      header={i18n.t('LoginModal.loginErrorHeader')}
+      content={i18n.t('LoginModal.loginErrorContent')}
+    />
+    <Grid
+      padded='vertically'
+      textAlign='center'
     >
-      <Header
-        icon='user circle'
-        content={props.t('LoginModal.header')}
-      />
-      <Message
-        error
-        header={props.t('LoginModal.loginErrorHeader')}
-        content={props.t('LoginModal.loginErrorContent')}
-      />
-      <Grid
-        padded='vertically'
-        textAlign='center'
+      <Grid.Column>
+        <Grid.Row
+          className='row'
+        >
+          <Input
+            autoFocus
+            className='form-field'
+            icon={<Icon name='at' />}
+            onChange={props.onUsernameChange.bind(this)}
+            placeholder={i18n.t('LoginModal.email')}
+            size='huge'
+          />
+        </Grid.Row>
+        <Grid.Row
+          className='row'
+        >
+          <Input
+            className='form-field'
+            icon={<Icon name='lock' />}
+            onChange={props.onPasswordChange.bind(this)}
+            placeholder={i18n.t('LoginModal.password')}
+            size='huge'
+            type='password'
+          />
+        </Grid.Row>
+      </Grid.Column>
+    </Grid>
+    <Modal.Actions>
+      <Button
+        disabled={props.disabled}
+        onClick={props.onLogin.bind(this)}
+        primary
+        size='large'
+        type='submit'
       >
-        <Grid.Column>
-          <Grid.Row
-            className='row'
-          >
-            <Input
-              autoFocus
-              className='form-field'
-              icon={<Icon name='at' />}
-              onChange={props.onUsernameChange.bind(this)}
-              placeholder={props.t('LoginModal.email')}
-              size='huge'
-            />
-          </Grid.Row>
-          <Grid.Row
-            className='row'
-          >
-            <Input
-              className='form-field'
-              icon={<Icon name='lock' />}
-              onChange={props.onPasswordChange.bind(this)}
-              placeholder={props.t('LoginModal.password')}
-              size='huge'
-              type='password'
-            />
-          </Grid.Row>
-        </Grid.Column>
-      </Grid>
-      <Modal.Actions>
-        <Button
-          disabled={props.disabled}
-          onClick={props.onLogin.bind(this)}
-          primary
-          size='large'
-          type='submit'
-        >
-          { props.t('LoginModal.buttonLogin') }
-        </Button>
-        <Button
-          inverted
-          onClick={props.onClose.bind(this)}
-          primary
-          size='large'
-        >
-          { props.t('LoginModal.buttonCancel') }
-        </Button>
-      </Modal.Actions>
-    </Modal>
-  </I18nextProvider>
+        { i18n.t('LoginModal.buttonLogin') }
+      </Button>
+      <Button
+        inverted
+        onClick={props.onClose.bind(this)}
+        primary
+        size='large'
+      >
+        { i18n.t('LoginModal.buttonCancel') }
+      </Button>
+    </Modal.Actions>
+  </Modal>
 );
 
-export default withTranslation()(LoginModal);
+export default LoginModal;
