03b9437cf2d4bfe29e400a2529e466398110358a
Derek Leadbetter <dleadbetter55@gmail.com>
2020-05-18 16:24:02 -0400

    Updating webpack config to reference new index.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   devtool: 'source-map',
-  entry: './src/index.js',
+  entry: 'index.js',
   externals: [nodeExternals()],
   output: {
     filename: 'index.js',
