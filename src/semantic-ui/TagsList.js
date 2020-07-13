03939c622b8d716a43834bfbc8ac0d6d2f1c294c
Derek Leadbetter <dleadbetter55@gmail.com>
2020-07-13 10:24:39 -0400

    CC #27 - Updating TagsList component to use the Label component from semantic ui; moving out of common package

---
+++ src/semantic-ui/TagsList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Label } from 'semantic-ui-react';
+import _ from 'underscore';
+import './TagsList.css';
+
+type Props = {
+  config: {
+    [key: string]: {
+      background: string,
+      color: string
+    }
+  },
+  tags: Array<string>
+};
+
+const TagsList = ({ config, tags = [] }: Props) => (
+  <div
+    className='tags-list'
+  >
+    { _.map(tags, (tag) => (
+      <Label
+        style={{
+          backgroundColor: config[tag] && config[tag].background,
+          color: config[tag] && config[tag].color,
+          marginRight: '10px'
+        }}
+      >
+        { tag }
+      </Label>
+    ))}
+  </div>
+);
+
+export default TagsList;
