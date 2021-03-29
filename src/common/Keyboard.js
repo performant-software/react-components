1c2f6edbfd4fc502d8df8f9c76bf1b96763f9573
Derek Leadbetter <dleadbetter55@gmail.com>
2021-03-29 16:43:27 -0400

    Archnet #249 - Upgrading simple-keyboard dependency

--- src/common/Keyboard.js
+++ src/common/Keyboard.js
@@ -33,7 +33,7 @@ const KeyboardSimple = (props: Props) => {
   // Sets up the keyboard reference and the initial value when the component is first rendered.
   useEffect(() => {
     const { layout, onChange } = props;
-    keyboardRef.current = new Keyboard({ layout, onChange, onKeyPress });
+    keyboardRef.current = new Keyboard({ ...layout, onChange, onKeyPress });
     keyboardRef.current.setInput(props.value);
   }, []);
 
