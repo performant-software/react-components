34db3598bd344082c053955ae3395b96de675b67
Chris Collins <chris@performantsoftware.com>
2021-06-08 14:48:33 -0400

    Fixes bug w/ multiple KeyboardField instances

--- stories/components/semantic-ui/KeyboardField.stories.js
+++ stories/components/semantic-ui/KeyboardField.stories.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { withA11y } from '@storybook/addon-a11y';
-import { withKnobs, boolean, text } from "@storybook/addon-knobs";
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
 import { Form } from 'semantic-ui-react';
 import English from 'simple-keyboard-layouts/build/layouts/english';
+import Arabic from 'simple-keyboard-layouts/build/layouts/arabic';
 import KeyboardField from '../../../src/semantic-ui/KeyboardField';
 
 export default {
@@ -11,7 +12,8 @@ export default {
 };
 
 export const Default = () => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState('a');
+  const [value2, setValue2] = useState('b');
 
   return (
     <Form>
@@ -22,7 +24,26 @@ export const Default = () => {
         onChange={(e, { value }) => setValue(value)}
         required={boolean('Required', false)}
         value={value}
-      />
+      >
+        <input
+          className='english'
+          type='text'
+        />
+      </KeyboardField>
+      <KeyboardField
+        label={text('Field name', 'Field')}
+        layout={Arabic}
+        name='test-field-2'
+        onChange={(e, { value }) => setValue2(value)}
+        required={boolean('Required', false)}
+        value={value2}
+      >
+        <input
+          className='arabic'
+          dir='rtl'
+          type='text'
+        />
+      </KeyboardField>
     </Form>
   );
 };
