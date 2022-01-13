3612c2f76eb94375b2e9d176a7ef3d084c7c141e
Derek Leadbetter <dleadbetter55@gmail.com>
2022-01-13 08:36:07 -0500

    FCA #12 - Refactoring GoogleAnalytics into a higher order component

--- src/common/GoogleAnalytics.js
+++ src/common/GoogleAnalytics.js
@@ -1,20 +1,126 @@
 // @flow
 
-import React from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useState,
+  type ComponentType
+} from 'react';
 import _ from 'underscore';
 
 type Props = {
-  id: string
+  id: string,
+  storageKey: string
 };
-
-const GA_CONSENT_KEY = 'ga_consent';
 
 const GoogleAnalyticsStatus = {
   accepted: 'accepted',
   notSet: 'not_set',
-  rejected: 'rejected',
+  rejected: 'rejected'
 };
 
+/**
+ * Returns the Google Analytics consent value from local storage.
+ *
+ * @param storageKey
+ *
+ * @returns {string|string}
+ */
+const getGoogleAnalyticsConsent = (storageKey) => localStorage.getItem(storageKey) || GoogleAnalyticsStatus.notSet;
+
+/**
+ * Sets the Google Analytics consent value in local storage.
+ *
+ * @param storageKey
+ * @param status
+ */
+const setGoogleAnalyticsConsent = (storageKey: string, status: string) => localStorage.setItem(storageKey, status);
+
+/**
+ * Sends the page view event to Google Analytics.
+ *
+ * @param id
+ * @param url
+ */
+const pageView = (id: string, storageKey: string, url: string) => {
+  const status = getGoogleAnalyticsConsent(storageKey);
+  if (status === GoogleAnalyticsStatus.accepted && window.gtag) {
+    window.gtag('config', id, { page_path: url });
+  }
+};
+
+/**
+ * Higher order component function used to wrap a "cookie consent" banner with Google Analytics.
+ *
+ * @param BannerComponent
+ *
+ * @returns {function(Props)}
+ */
+const withGoogleAnalytics = (BannerComponent: ComponentType<any>) => (props: Props) => {
+  const [status, setStatus] = useState(GoogleAnalyticsStatus.notSet);
+
+  /**
+   * Sets the "accepted" status and initializes analytics.
+   *
+   * @type {(function(): void)|*}
+   */
+  const onAccept = useCallback(() => {
+    setStatus(GoogleAnalyticsStatus.accepted);
+
+    if (window.initializeAnalytics) {
+      window.initializeAnalytics();
+    }
+  }, []);
+
+  /**
+   * Sets the "rejected" status.
+   *
+   * @type {(function(): void)|*}
+   */
+  const onDecline = useCallback(() => {
+    setStatus(GoogleAnalyticsStatus.rejected);
+  }, []);
+
+  /**
+   * Sets the initial status on the state from local storage.
+   */
+  useEffect(() => {
+    setStatus(getGoogleAnalyticsConsent(props.storageKey));
+  }, [props.storageKey]);
+
+  /**
+   * Sets the status on local storage when the state changes.
+   */
+  useEffect(() => {
+    setGoogleAnalyticsConsent(props.storageKey, status);
+  }, [status, props.storageKey]);
+
+  return (
+    <>
+      <GoogleAnalyticsScript
+        id={props.id}
+        storageKey={props.storageKey}
+      />
+      { status === GoogleAnalyticsStatus.notSet && (
+        <BannerComponent
+          {...props}
+          onAccept={onAccept}
+          onDecline={onDecline}
+        />
+      )}
+    </>
+  );
+};
+
+/**
+ * Renders the component to insert the necessary scripts for Google Analytics.
+ *
+ * @param props
+ *
+ * @returns {JSX.Element|null}
+ *
+ * @constructor
+ */
 const GoogleAnalyticsScript = (props: Props) => {
   // Only include the script if an API key is provided.
   if (_.isEmpty(props.id)) {
@@ -30,8 +136,8 @@ const GoogleAnalyticsScript = (props: Props) => {
       <script
         dangerouslySetInnerHTML={{
           __html: `
-            const cookies = localStorage.getItem('${GA_CONSENT_KEY}');
-            
+            const cookies = localStorage.getItem('${props.storageKey}');
+
             window.initializeAnalytics = () => {
               window.dataLayer = window.dataLayer || [];
               function gtag() { dataLayer.push(arguments); }
@@ -40,7 +146,7 @@ const GoogleAnalyticsScript = (props: Props) => {
                 page_path: window.location.pathname,
               });
             };
-            
+
             if (cookies === '${GoogleAnalyticsStatus.accepted}') {
               window.initializeAnalytics();
             }
@@ -51,40 +157,8 @@ const GoogleAnalyticsScript = (props: Props) => {
   );
 };
 
-/**
- * Returns the "ga_consent" value from local storage.
- *
- * @returns {?string}
- */
-const getGoogleAnalyticsConsent = () => localStorage.getItem(GA_CONSENT_KEY);
-
-/**
- * Sets the "ga_consent" value on local storage.
- *
- * @param status
- */
-const setGoogleAnalyticsConsent = (status: string) => localStorage.setItem(GA_CONSENT_KEY, status);
-
-/**
- * Sends the page view event to Google Analytics.
- *
- * @param id
- * @param url
- */
-const pageView = (id: string, url: string) => {
-  const cookies = getGoogleAnalyticsConsent();
-  if (cookies === GoogleAnalyticsStatus.accepted && window.gtag) {
-    window.gtag('config', id, { page_path: url });
-  }
-};
-
-export default {
-  pageView,
-  getGoogleAnalyticsConsent,
-  setGoogleAnalyticsConsent
-};
+export default withGoogleAnalytics;
 
 export {
-  GoogleAnalyticsScript,
-  GoogleAnalyticsStatus
+  pageView
 };
