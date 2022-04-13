a3f0e233f52d79365e00daaf40e5630f65f9ee69
Derek Leadbetter <dleadbetter55@gmail.com>
2022-04-13 08:45:28 -0400

    RC #122 - Adding plugin options to webpack config

--- packages/webpack/index.js
+++ packages/webpack/index.js
@@ -54,6 +54,14 @@ module.exports = {
                   '@babel/plugin-proposal-class-properties', {
                     loose: true
                   }
+                ], [
+                  '@babel/plugin-proposal-private-methods', {
+                    loose: true
+                  }
+                ], [
+                  '@babel/plugin-proposal-private-property-in-object', {
+                    loose: true
+                  }
                 ]
               ],
               ignore: [
