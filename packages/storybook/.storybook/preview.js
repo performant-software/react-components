a1c36984caf95a9fafd21708e035d9e73a09212f
Derek Leadbetter <dleadbetter55@gmail.com>
2022-05-18 15:16:42 -0400

    RC #133 - Adding ability to specify environment variable for accessibility level

--- packages/storybook/.storybook/preview.js
+++ packages/storybook/.storybook/preview.js
@@ -10,7 +10,26 @@ addParameters({
   }
 });
 
+/**
+ * If a list of accessibility tags are provided, only run the tests for those specific tags.
+ *
+ * @see https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#axe-core-tags
+ */
+let a11y;
+
+if (process.env.A11Y_TAGS) {
+  a11y = {
+    config: {
+      runOnly: {
+        type: 'tag',
+        values: process.env.A11Y_TAGS.split(',')
+      }
+    }
+  }
+}
+
 export const parameters = {
+  a11y,
   options: {
     storySort: {
       order: ['Overview', 'Components']
