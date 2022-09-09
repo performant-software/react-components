40e36780d06fadece9cb6af4b4a1c739bc414ea2
Derek Leadbetter <dleadbetter55@gmail.com>
2022-09-09 15:12:18 -0400

    UDCSL #63 - Creating "visualize" package and TreeGraph/Zoom components

---
+++ packages/storybook/src/visualize/TreeGraph.stories.js
@@ -0,0 +1,97 @@
+// @flow
+
+import React, { useCallback, useState } from 'react';
+import { withA11y } from '@storybook/addon-a11y';
+import { select, withKnobs } from '@storybook/addon-knobs';
+import _ from 'underscore';
+import TreeGraph from '../../../visualize/src/components/TreeGraph';
+
+export default {
+  title: 'Components/Visualize/TreeGraph',
+  decorators: [withA11y, withKnobs]
+};
+
+const data = {
+  name: 'T',
+  isExpanded: true,
+  children: [
+    {
+      name: 'A',
+      children: [{
+        name: 'A1'
+      }, {
+        name: 'A2'
+      }, {
+        name: 'A3'
+      }, {
+        name: 'C',
+        children: [{
+          name: 'C1',
+        }, {
+          name: 'D',
+          children: [{
+            name: 'D1',
+          }, {
+            name: 'D2',
+          }, {
+            name: 'D3',
+          }],
+        }],
+      }],
+    },
+    {
+      name: 'Z'
+    }, {
+      name: 'B',
+      children: [{
+        name: 'B1'
+      }, {
+        name: 'B2'
+      }, {
+        name: 'B3'
+      }]
+    }
+  ]
+};
+
+export const Default = () => {
+  const [items, setItems] = useState(data);
+
+  const { clientHeight: height, clientWidth: width } = document.body;
+
+  const updateNode = useCallback((node, item) => {
+    let updated;
+
+    if (item.name === node.name) {
+      updated = ({
+        ...item,
+        ...node,
+        isExpanded: !node.isExpanded
+      });
+    } else {
+      updated = ({
+        ...item,
+        children: _.map(item.children, (child) => updateNode(node, child))
+      });
+    }
+
+    return updated;
+  }, []);
+
+  return (
+    <div
+      style={{
+        height,
+        width
+      }}
+    >
+      <TreeGraph
+        data={items}
+        layout={select('Layout', ['cartesian', 'polar'])}
+        linkType={select('Link Type', ['curve', 'line', 'step'])}
+        onClick={(node) => setItems(updateNode(node, items))}
+        orientation={select('Orientation', ['horizontal', 'vertical'])}
+      />
+    </div>
+  );
+};
---
+++ packages/visualize/src/index.js
@@ -0,0 +1,7 @@
+// @flow
+
+// Components
+export { default as TreeGraph } from './components/TreeGraph';
+
+// Hooks
+export { default as withZoom } from './hooks/Zoom';
