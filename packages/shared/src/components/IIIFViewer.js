4aab9939bcc5e96a06cf12aced424d18f11c3fd9
Derek Leadbetter <dleadbetter55@gmail.com>
2022-10-31 10:53:27 -0400

    UDCSL #77 - Updating IIIFViewer to compile in Node environment

--- packages/shared/src/components/IIIFViewer.js
+++ packages/shared/src/components/IIIFViewer.js
@@ -2,11 +2,18 @@
 
 import React from 'react';
 import CloverIIIF from '@samvera/clover-iiif';
+import Browser from '../utils/Browser';
 
-const IIIFViewer = (props) => (
-  <CloverIIIF
-    {...props}
-  />
-);
+const IIIFViewer = (props) => {
+  if (!Browser.isBrowser()) {
+    return null;
+  }
+
+  return (
+    <CloverIIIF
+      {...props}
+    />
+  );
+};
 
 export default IIIFViewer;
