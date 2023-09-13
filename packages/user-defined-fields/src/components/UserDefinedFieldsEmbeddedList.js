3355819596e84fdb64fe8fe860e6b578f1883c2c
dleadbetter <>
2023-09-13 06:53:59 -0400

    CDC #13 - Adding modal.defaults and modal.props fields to UserDefinedFieldsEmbeddedList and UserDefinedFieldsList

--- packages/user-defined-fields/src/components/UserDefinedFieldsEmbeddedList.js
+++ packages/user-defined-fields/src/components/UserDefinedFieldsEmbeddedList.js
@@ -1,56 +1,68 @@
 // @flow
 
 import { BooleanIcon, EmbeddedList } from '@performant-software/semantic-components';
-import React from 'react';
+import React, { useMemo } from 'react';
 import i18n from '../i18n/i18n';
 import UserDefinedFieldModal from './UserDefinedFieldModal';
 import UserDefinedFields from '../utils/UserDefinedFields';
 
 type Props = {
+  defaults: any,
   items: Array<any>,
+  modal?: {
+    defaults: any,
+    props: any
+  },
   onDelete: (item: any) => Promise<any>,
   onSave: (item: any) => Promise<any>
 };
 
 const DEFAULT_ORDER = 0;
 
-const UserDefinedFieldsEmbeddedList = (props: Props) => (
-  <EmbeddedList
-    actions={[{
-      name: 'edit'
-    }, {
-      name: 'delete'
-    }]}
-    columns={[{
-      name: 'table_name',
-      label: i18n.t('UserDefinedFieldsEmbeddedList.columns.table')
-    }, {
-      name: 'column_name',
-      label: i18n.t('UserDefinedFieldsEmbeddedList.columns.name')
-    }, {
-      name: 'data_type',
-      label: i18n.t('UserDefinedFieldsEmbeddedList.columns.dataType')
-    }, {
-      name: 'required',
-      label: i18n.t('UserDefinedFieldsEmbeddedList.columns.required'),
-      render: (udf) => <BooleanIcon value={udf.required} />
-    }, {
-      name: 'order',
-      label: i18n.t('UserDefinedFieldsEmbeddedList.columns.order')
-    }]}
-    items={props.items}
-    modal={{
-      component: UserDefinedFieldModal,
-      props: {
-        defaults: {
-          order: DEFAULT_ORDER
-        },
-        validate: UserDefinedFields.validateUserDefinedField.bind(this)
-      }
-    }}
-    onDelete={props.onDelete}
-    onSave={props.onSave}
-  />
-);
+const UserDefinedFieldsEmbeddedList = (props: Props) => {
+  const defaults = useMemo(() => props.modal?.defaults || {}, [props.modal]);
+  const modalProps = useMemo(() => props.modal?.props || {}, [props.modal]);
+
+  return (
+    <EmbeddedList
+      actions={[{
+        name: 'edit'
+      }, {
+        name: 'delete'
+      }]}
+      columns={[{
+        name: 'table_name',
+        label: i18n.t('UserDefinedFieldsEmbeddedList.columns.table')
+      }, {
+        name: 'column_name',
+        label: i18n.t('UserDefinedFieldsEmbeddedList.columns.name')
+      }, {
+        name: 'data_type',
+        label: i18n.t('UserDefinedFieldsEmbeddedList.columns.dataType')
+      }, {
+        name: 'required',
+        label: i18n.t('UserDefinedFieldsEmbeddedList.columns.required'),
+        render: (udf) => <BooleanIcon value={udf.required} />
+      }, {
+        name: 'order',
+        label: i18n.t('UserDefinedFieldsEmbeddedList.columns.order')
+      }]}
+      items={props.items}
+      modal={{
+        component: UserDefinedFieldModal,
+        props: {
+          defaults: {
+            order: DEFAULT_ORDER,
+            ...defaults
+          },
+          ...modalProps,
+          validate: UserDefinedFields.validateUserDefinedField.bind(this)
+        }
+      }}
+      onDelete={props.onDelete}
+      onSave={props.onSave}
+    />
+  );
+};
 
 export default UserDefinedFieldsEmbeddedList;
--- packages/user-defined-fields/src/components/UserDefinedFieldsList.js
+++ packages/user-defined-fields/src/components/UserDefinedFieldsList.js
@@ -1,52 +1,66 @@
 // @flow
 
 import { BooleanIcon, ListTable } from '@performant-software/semantic-components';
-import React, { type ComponentType } from 'react';
+import React, { type ComponentType, useMemo } from 'react';
 import i18n from '../i18n/i18n';
 import UserDefinedFieldModal from './UserDefinedFieldModal';
 import UserDefinedFields from '../utils/UserDefinedFields';
 import UserDefinedFieldsService from '../services/UserDefinedFields';
 
+type Props = {
+  modal?: {
+    defaults: any,
+    props: any
+  }
+};
+
 const DEFAULT_ORDER = 0;
 
-const UserDefinedFieldsList: ComponentType<any> = () => (
-  <ListTable
-    actions={[{
-      name: 'edit'
-    }, {
-      name: 'delete'
-    }]}
-    columns={[{
-      name: 'table_name',
-      label: i18n.t('UserDefinedFieldsList.columns.table')
-    }, {
-      name: 'column_name',
-      label: i18n.t('UserDefinedFieldsList.columns.name')
-    }, {
-      name: 'data_type',
-      label: i18n.t('UserDefinedFieldsList.columns.dataType')
-    }, {
-      name: 'required',
-      label: i18n.t('UserDefinedFieldsList.columns.required'),
-      render: (udf) => <BooleanIcon value={udf.required} />
-    }, {
-      name: 'order',
-      label: i18n.t('UserDefinedFieldsList.columns.order')
-    }]}
-    collectionName='user_defined_fields'
-    modal={{
-      component: UserDefinedFieldModal,
-      props: {
-        defaults: {
-          order: DEFAULT_ORDER
-        },
-        validate: UserDefinedFields.validateUserDefinedField.bind(this)
-      }
-    }}
-    onLoad={(params) => UserDefinedFieldsService.fetchAll(params)}
-    onSave={(udf) => UserDefinedFieldsService.save(udf)}
-    onDelete={(udf) => UserDefinedFieldsService.delete(udf)}
-  />
-);
+const UserDefinedFieldsList: ComponentType<any> = (props: Props) => {
+  const defaults = useMemo(() => props.modal?.defaults || {}, [props.modal]);
+  const modalProps = useMemo(() => props.modal?.props || {}, [props.modal]);
+
+  return (
+    <ListTable
+      actions={[{
+        name: 'edit'
+      }, {
+        name: 'delete'
+      }]}
+      columns={[{
+        name: 'table_name',
+        label: i18n.t('UserDefinedFieldsList.columns.table')
+      }, {
+        name: 'column_name',
+        label: i18n.t('UserDefinedFieldsList.columns.name')
+      }, {
+        name: 'data_type',
+        label: i18n.t('UserDefinedFieldsList.columns.dataType')
+      }, {
+        name: 'required',
+        label: i18n.t('UserDefinedFieldsList.columns.required'),
+        render: (udf) => <BooleanIcon value={udf.required} />
+      }, {
+        name: 'order',
+        label: i18n.t('UserDefinedFieldsList.columns.order')
+      }]}
+      collectionName='user_defined_fields'
+      modal={{
+        component: UserDefinedFieldModal,
+        props: {
+          defaults: {
+            order: DEFAULT_ORDER,
+            ...defaults
+          },
+          ...modalProps,
+          validate: UserDefinedFields.validateUserDefinedField.bind(this)
+        }
+      }}
+      onLoad={(params) => UserDefinedFieldsService.fetchAll(params)}
+      onSave={(udf) => UserDefinedFieldsService.save(udf)}
+      onDelete={(udf) => UserDefinedFieldsService.delete(udf)}
+    />
+  );
+};
 
 export default UserDefinedFieldsList;
