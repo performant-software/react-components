88dec0dc4b2fa7a2d0285062c43ba023e8522f7a
Rebecca Black <rebecca.ann.black@gmail.com>
2023-09-27 12:24:56 -0400

    updates to SaveButton docs and story

---
+++ packages/storybook/src/semantic-ui/SaveButton.stories.js
@@ -0,0 +1,43 @@
+// @flow
+
+import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import SaveButton from '../../../semantic-ui/src/components/SaveButton';
+
+export default {
+  title: 'Components/Semantic UI/SaveButton',
+  component: SaveButton
+};
+
+export const Default = () => {
+    const [msg, setMsg] = useState('');
+return (
+<div>
+  <SaveButton
+    onClick={() => setMsg('You have saved!')}
+  />
+  <p>{msg}</p>
+</div>
+)};
+
+export const WithDelay = () => {
+    const [saving, setSaving] = useState(false);
+    const [msg, setMsg] = useState('');
+
+    const onSave = () => {
+        setSaving(true)
+        setTimeout(() => {
+            setMsg('You have saved!');
+            setSaving(false);
+        }, 3000);
+    }
+    return (
+    <div>
+    <SaveButton
+     onClick={onSave}
+     saving={saving}
+    />
+    <p>{msg}</p>
+    </div>
+)};
