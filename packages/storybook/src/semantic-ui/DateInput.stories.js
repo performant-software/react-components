feebf5599635078315174d5b09c62669e4360173
Ben Silverman <ben@performantsoftware.com>
2023-09-27 15:13:39 -0400

    Add stories for DateInput component

---
+++ packages/storybook/src/semantic-ui/DateInput.stories.js
@@ -0,0 +1,63 @@
+// @flow
+
+import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
+import DateInput from '../../../semantic-ui/src/components/DateInput';
+
+export default {
+  title: 'Components/Semantic UI/DateInput'
+};
+
+export const Default = () => {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <DateInput
+      onChange={(d) => setDate(d)}
+      onClick={action('click')}
+      value={date}
+    />
+  );
+};
+
+export const WithLocale = () => {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <DateInput
+      locale='de'
+      onChange={(d) => setDate(d)}
+      onClick={action('click')}
+      value={date}
+    />
+  );
+};
+
+export const WithLocaleAndFormatOptions = () => {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <DateInput
+      formatOptions={{
+        weekday: 'long', day: 'numeric', month: 'numeric', hour: 'numeric'
+      }}
+      locale='fr'
+      onChange={(d) => setDate(d)}
+      onClick={action('click')}
+      value={date}
+    />
+  );
+};
+
+export const WithDisplay = () => {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <DateInput
+      display={date ? `Today is ${date}` : 'Cleared!'}
+      onChange={(d) => setDate(d)}
+      onClick={action('click')}
+      value={date}
+    />
+  );
+};
