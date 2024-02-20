f809bb469f0a5d65ca336100639d8cde591151ed
lorinjameson <lwjameson@gmail.com>
2024-02-20 08:34:54 -0500

    WIP

--- packages/storybook/src/semantic-ui/VideoFrameSelector.stories.js
+++ packages/storybook/src/semantic-ui/VideoFrameSelector.stories.js
@@ -3,7 +3,9 @@
 import React, { useState } from 'react';
 import LazyVideo from '../../../semantic-ui/src/components/LazyVideo';
 import VideoFrameSelector from '../../../semantic-ui/src/components/VideoFrameSelector';
-import video from '../assets/SampleVideo.mp4';
+// import video from '../assets/SampleVideo.mp4';
+
+const video = 'https://file-examples.com/storage/fe9379915f65d3c76997f7c/2017/04/file_example_MP4_1920_18MG.mp4';
 
 export default {
   title: 'Components/Semantic UI/VideoFrameSelector'
