19b819386e73dac74bf948fa36dc81ce3ce11f98
Rebecca Black <rebecca.ann.black@gmail.com>
2024-03-11 10:20:06 -0400

    added tailwind to storybook package

---
+++ packages/storybook/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/index.css',
+    './src/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
