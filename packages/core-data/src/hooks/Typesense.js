51b54ec6d5589d096efec7e9a3600fae31ed6afc
dleadbetter <>
2024-03-21 11:45:35 -0400

    Atlas #4 - Adding useGeoSearchToggle hook

--- packages/core-data/src/hooks/Typesense.js
+++ packages/core-data/src/hooks/Typesense.js
@@ -1,6 +1,12 @@
 // @flow
 
-import { useContext, useEffect } from 'react';
+import { useMap } from '@peripleo/maplibre';
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useState
+} from 'react';
 import PersistentSearchStateContext from '../context/PersistentSearchState';
 
 export const useCachedHits = () => {
@@ -28,4 +34,55 @@ export const useSearchCompleted = (callback: ((hits: TypesenseSearchResult[]) =>
       unobserve(callback);
     };
   }, deps);
+};
+
+export const useGeoSearchToggle = () => {
+  const { clearMapRefinement, isRefinedWithMap, refine } = useGeoSearch();
+
+  const [filterByMapBounds, setFilterByMapBounds] = useState(isRefinedWithMap());
+
+  const map = useMap();
+
+  const onChangeViewport = useCallback(() => {
+    const bounds = map.getBounds();
+
+    const northEast = bounds.getNorthEast();
+    const southWest = bounds.getSouthWest();
+
+    // Note TypeSense seems to be buggy. If the bounds are too large,
+    // it will return 0 results. (Note that lng values >180, <(-180) DO NOT
+    // seem to be the problem!)
+    const extent = northEast.lng - southWest.lng;
+    if (extent > 200) {
+      clearMapRefinement();
+    } else {
+      refine({ northEast, southWest });
+    }
+  }, [map]);
+
+  useEffect(() => {
+    if (!map) {
+      return null;
+    }
+
+    if (filterByMapBounds) {
+      onChangeViewport(); // Initial filter
+
+      map.on('dragend', onChangeViewport);
+      map.on('zoomend', onChangeViewport);
+
+      return () => {
+        map.off('dragend', onChangeViewport);
+        map.off('zoomend', onChangeViewport);
+      };
+    }
+
+    clearMapRefinement();
+    return null;
+  }, [map, onChangeViewport, filterByMapBounds]);
+
+  return {
+    filterByMapBounds,
+    setFilterByMapBounds
+  };
 };
