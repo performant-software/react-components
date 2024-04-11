16802e33781b53f450d0372a7daea65cf0d37428
lorinjameson <lwjameson@gmail.com>
2024-04-11 14:08:26 -0400

    First pass implementation on where in

--- packages/semantic-ui/src/components/ListFilters.js
+++ packages/semantic-ui/src/components/ListFilters.js
@@ -1,12 +1,7 @@
 // @flow
 
-import { type EditContainerProps } from '@performant-software/shared-components';
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  type Element
-} from 'react';
+import { type EditContainerProps } from "@performant-software/shared-components";
+import React, { useCallback, useEffect, useMemo, type Element } from "react";
 import {
   Button,
   Checkbox,
@@ -15,24 +10,24 @@ import {
   Grid,
   Header,
   Input,
-  Modal
-} from 'semantic-ui-react';
-import _ from 'underscore';
-import i18n from '../i18n/i18n';
-import AssociatedDropdown from './AssociatedDropdown';
-import DropdownButton from './DropdownButton';
-import FuzzyDate from './FuzzyDate';
-import ModalContext from '../context/ModalContext';
+  Modal,
+} from "semantic-ui-react";
+import _ from "underscore";
+import i18n from "../i18n/i18n";
+import AssociatedDropdown from "./AssociatedDropdown";
+import DropdownButton from "./DropdownButton";
+import FuzzyDate from "./FuzzyDate";
+import ModalContext from "../context/ModalContext";
 
 type Option = {
   key: string | number,
   icon?: string,
   text: string,
-  value: any
+  value: any,
 };
 
 type Filter = {
-  collectionName?: 'string',
+  collectionName?: "string",
   key: string,
   label: string,
   onSearch?: (search: string) => Promise<any>,
@@ -42,97 +37,103 @@ type Filter = {
   renderSearchQuery?: (item: any) => Element<any> | string,
   searchQuery?: string,
   type: string,
-  value: ?any
+  value: ?any,
 };
 
 type Props = EditContainerProps & {
   filters: Array<Filter>,
   item: {
     filters: Array<Filter>,
-    onSort: (filter: Filter) => any
+    onSort: (filter: Filter) => any,
   },
-  onCreateFilter: (filter: Filter) => any
+  onCreateFilter: (filter: Filter) => any,
 };
 
 const FilterTypes = {
-  boolean: 'boolean',
-  date: 'date',
-  integer: 'integer',
-  relationship: 'relationship',
-  select: 'select',
-  string: 'string',
-  text: 'text'
+  boolean: "boolean",
+  date: "date",
+  integer: "integer",
+  relationship: "relationship",
+  select: "select",
+  string: "string",
+  text: "text",
 };
 
 const FilterOperators = {
-  equal: 'equal',
-  notEqual: 'not_equal',
-  contain: 'contain',
-  notContain: 'not_contain',
-  empty: 'empty',
-  notEmpty: 'not_empty',
-  greaterThan: 'greater_than',
-  lessThan: 'less_than',
-  whereIn: 'where_in'
+  equal: "equal",
+  notEqual: "not_equal",
+  contain: "contain",
+  notContain: "not_contain",
+  empty: "empty",
+  notEmpty: "not_empty",
+  greaterThan: "greater_than",
+  lessThan: "less_than",
+  whereIn: "where_in",
 };
 
-const FilterOperatorOptions = [{
-  key: FilterOperators.equal,
-  value: FilterOperators.equal,
-  text: i18n.t('ListFilters.operators.equal')
-}, {
-  key: FilterOperators.notEqual,
-  value: FilterOperators.notEqual,
-  text: i18n.t('ListFilters.operators.notEqual')
-}, {
-  key: FilterOperators.contain,
-  value: FilterOperators.contain,
-  text: i18n.t('ListFilters.operators.contain')
-}, {
-  key: FilterOperators.notContain,
-  value: FilterOperators.notContain,
-  text: i18n.t('ListFilters.operators.notContain')
-}, {
-  key: FilterOperators.empty,
-  value: FilterOperators.empty,
-  text: i18n.t('ListFilters.operators.empty')
-}, {
-  key: FilterOperators.notEmpty,
-  value: FilterOperators.notEmpty,
-  text: i18n.t('ListFilters.operators.notEmpty')
-}, {
-  key: FilterOperators.greaterThan,
-  value: FilterOperators.greaterThan,
-  text: i18n.t('ListFilters.operators.greaterThan')
-}, {
-  key: FilterOperators.lessThan,
-  value: FilterOperators.lessThan,
-  text: i18n.t('ListFilters.operators.lessThan')
-}, {
-  key: FilterOperators.whereIn,
-  value: FilterOperators.whereIn,
-  text: i18n.t('ListFilters.operators.whereIn')
-}];
+const FilterOperatorOptions = [
+  {
+    key: FilterOperators.equal,
+    value: FilterOperators.equal,
+    text: i18n.t("ListFilters.operators.equal"),
+  },
+  {
+    key: FilterOperators.notEqual,
+    value: FilterOperators.notEqual,
+    text: i18n.t("ListFilters.operators.notEqual"),
+  },
+  {
+    key: FilterOperators.contain,
+    value: FilterOperators.contain,
+    text: i18n.t("ListFilters.operators.contain"),
+  },
+  {
+    key: FilterOperators.notContain,
+    value: FilterOperators.notContain,
+    text: i18n.t("ListFilters.operators.notContain"),
+  },
+  {
+    key: FilterOperators.empty,
+    value: FilterOperators.empty,
+    text: i18n.t("ListFilters.operators.empty"),
+  },
+  {
+    key: FilterOperators.notEmpty,
+    value: FilterOperators.notEmpty,
+    text: i18n.t("ListFilters.operators.notEmpty"),
+  },
+  {
+    key: FilterOperators.greaterThan,
+    value: FilterOperators.greaterThan,
+    text: i18n.t("ListFilters.operators.greaterThan"),
+  },
+  {
+    key: FilterOperators.lessThan,
+    value: FilterOperators.lessThan,
+    text: i18n.t("ListFilters.operators.lessThan"),
+  },
+  {
+    key: FilterOperators.whereIn,
+    value: FilterOperators.whereIn,
+    text: i18n.t("ListFilters.operators.whereIn"),
+  },
+];
 
 const OperatorsByType = {
-  [FilterTypes.boolean]: [
-    FilterOperators.equal
-  ],
-  [FilterTypes.date]: [
-    FilterOperators.equal,
-    FilterOperators.whereIn
-  ],
+  [FilterTypes.boolean]: [FilterOperators.equal],
+  [FilterTypes.date]: [FilterOperators.equal, FilterOperators.whereIn],
   [FilterTypes.relationship]: [
     FilterOperators.equal,
     FilterOperators.notEqual,
     FilterOperators.empty,
-    FilterOperators.notEmpty
+    FilterOperators.notEmpty,
+    FilterOperators.whereIn,
   ],
   [FilterTypes.select]: [
     FilterOperators.equal,
     FilterOperators.notEqual,
     FilterOperators.empty,
-    FilterOperators.notEmpty
+    FilterOperators.notEmpty,
   ],
   [FilterTypes.string]: [
     FilterOperators.equal,
@@ -141,14 +142,14 @@ const OperatorsByType = {
     FilterOperators.notContain,
     FilterOperators.empty,
     FilterOperators.notEmpty,
-    FilterOperators.whereIn
+    FilterOperators.whereIn,
   ],
   [FilterTypes.text]: [
     FilterOperators.contain,
     FilterOperators.notContain,
     FilterOperators.empty,
-    FilterOperators.notEmpty
-  ]
+    FilterOperators.notEmpty,
+  ],
 };
 
 const ListFilters = (props: Props) => {
@@ -159,7 +160,10 @@ const ListFilters = (props: Props) => {
    */
   const getOperatorsByType = useCallback((type: string) => {
     const operators = OperatorsByType[type];
-    return _.filter(FilterOperatorOptions, (option) => !operators || _.contains(operators, option.key));
+    return _.filter(
+      FilterOperatorOptions,
+      (option) => !operators || _.contains(operators, option.key)
+    );
   }, []);
 
   /**
@@ -174,108 +178,133 @@ const ListFilters = (props: Props) => {
    *
    * @type {(function(Filter): (null|*))|*}
    */
-  const renderInput = useCallback((filter: Filter) => {
-    // No need to render an input for "empty" or "not empty" operators
-    if (filter.operator === FilterOperators.empty || filter.operator === FilterOperators.notEmpty) {
-      return null;
-    }
-
-    // Render a checkbox for boolean types
-    if (filter.type === FilterTypes.boolean) {
-      return (
-        <Checkbox
-          onChange={() => props.onSaveChildAssociation('filters', { ...filter, value: !filter.value })}
-          checked={filter.value}
-        />
-      );
-    }
-
-    // Render a text input for string, text, and integer types
-    if (filter.type === FilterTypes.string || filter.type === FilterTypes.text || filter.type === FilterTypes.integer) {
-      return (
-        <Input
-          onChange={(e, { value }) => props.onSaveChildAssociation('filters', { ...filter, value })}
-          value={filter.value}
-        />
-      );
-    }
-
-    // Render a fuzzy date input for date types
-    if (filter.type === FilterTypes.date) {
-      return (
-        <FuzzyDate
-          date={filter.value || {}}
-          description={false}
-          onChange={(value) => {
-            props.onSaveChildAssociation('filters', {
-              ...filter,
-              value
-            });
-          }}
-          title={filter.label}
-        />
-      );
-    }
-
-    // Render a dropdown
-    if (filter.type === FilterTypes.select) {
-      return (
-        <Dropdown
-          onChange={(e, { value }) => props.onSaveChildAssociation('filters', { ...filter, value })}
-          options={filter.options}
-          selectOnBlur={false}
-          selection
-          value={filter.value}
-        />
-      );
-    }
-
-    // Render an AssociatedDropdown for relationship types
-    if (filter.type === FilterTypes.relationship) {
-      const {
-        collectionName,
-        onSearch,
-        renderOption,
-        renderSearchQuery,
-        searchQuery,
-        value
-      } = filter;
-
-      if (!(collectionName && onSearch && renderOption)) {
+  const renderInput = useCallback(
+    (filter: Filter) => {
+      // No need to render an input for "empty" or "not empty" operators
+      if (
+        filter.operator === FilterOperators.empty ||
+        filter.operator === FilterOperators.notEmpty
+      ) {
         return null;
       }
 
-      return (
-        <AssociatedDropdown
-          collectionName={collectionName}
-          onSearch={onSearch}
-          onSelection={(item) => {
-            props.onSaveChildAssociation('filters', {
-              ...filter,
-              searchQuery: renderSearchQuery && renderSearchQuery(item),
-              value: item.id
-            });
-          }}
-          renderOption={renderOption}
-          required
-          searchQuery={searchQuery || ''}
-          value={value || null}
-        />
-      );
-    }
+      // Render a checkbox for boolean types
+      if (filter.type === FilterTypes.boolean) {
+        return (
+          <Checkbox
+            onChange={() =>
+              props.onSaveChildAssociation("filters", {
+                ...filter,
+                value: !filter.value,
+              })
+            }
+            checked={filter.value}
+          />
+        );
+      }
 
-    return null;
-  }, [props.item.filters, props.onSaveChildAssociation]);
+      // Render a text input for string, text, and integer types
+      if (
+        filter.type === FilterTypes.string ||
+        filter.type === FilterTypes.text ||
+        filter.type === FilterTypes.integer
+      ) {
+        return (
+          <Input
+            onChange={(e, { value }) =>
+              props.onSaveChildAssociation("filters", { ...filter, value })
+            }
+            value={filter.value}
+          />
+        );
+      }
+
+      // Render a fuzzy date input for date types
+      if (filter.type === FilterTypes.date) {
+        return (
+          <FuzzyDate
+            date={filter.value || {}}
+            description={false}
+            onChange={(value) => {
+              props.onSaveChildAssociation("filters", {
+                ...filter,
+                value,
+              });
+            }}
+            title={filter.label}
+          />
+        );
+      }
+
+      // Render a dropdown
+      if (filter.type === FilterTypes.select) {
+        return (
+          <Dropdown
+            onChange={(e, { value }) =>
+              props.onSaveChildAssociation("filters", { ...filter, value })
+            }
+            options={filter.options}
+            selectOnBlur={false}
+            selection
+            value={filter.value}
+          />
+        );
+      }
+
+      // Render an AssociatedDropdown for relationship types
+      if (filter.type === FilterTypes.relationship) {
+        const {
+          collectionName,
+          onSearch,
+          renderOption,
+          renderSearchQuery,
+          searchQuery,
+          operator,
+          value,
+        } = filter;
+
+        if (!(collectionName && onSearch && renderOption)) {
+          return null;
+        }
+
+        return (
+          <AssociatedDropdown
+            collectionName={collectionName}
+            onSearch={onSearch}
+            onSelection={(item) => {
+              props.onSaveChildAssociation("filters", {
+                ...filter,
+                searchQuery:
+                  renderSearchQuery &&
+                  operator !== FilterOperators.whereIn &&
+                  renderSearchQuery(item),
+                value: operator !== FilterOperators.whereIn ? item.id : item,
+              });
+            }}
+            renderOption={renderOption}
+            required
+            searchQuery={searchQuery || ""}
+            value={value || (operator === FilterOperators.whereIn ? [] : null)}
+            multiple={operator === FilterOperators.whereIn}
+            key={operator}
+          />
+        );
+      }
+
+      return null;
+    },
+    [props.item.filters, props.onSaveChildAssociation]
+  );
 
   /**
    * Sort the filters according to the onSort prop if provided.
    *
    * @type {unknown}
    */
-  const filters = useMemo(() => _.sortBy(
-    props.filters,
-    props.item.onSort || onDefaultSort
-  ), [props.filters, props.item.onSort]);
+  const filters = useMemo(
+    () => _.sortBy(props.filters, props.item.onSort || onDefaultSort),
+    [props.filters, props.item.onSort]
+  );
 
   /**
    * Since the filters may be restored from the session storage, complex object and functions are not serialized. Here
@@ -289,106 +318,91 @@ const ListFilters = (props: Props) => {
         defaults.value = false;
       }
 
-      props.onSaveChildAssociation('filters', _.defaults(filter, defaults));
+      props.onSaveChildAssociation("filters", _.defaults(filter, defaults));
     });
   }, []);
 
   return (
     <ModalContext.Consumer>
-      { (mountNode) => (
+      {(mountNode) => (
         <Modal
           as={Form}
           centered={false}
-          className='list-filters-modal'
+          className="list-filters-modal"
           mountNode={mountNode}
           noValidate
           open
-          size='small'
+          size="small"
         >
           <Modal.Header>
-            <Grid
-              columns={2}
-            >
-              <Grid.Column
-                verticalAlign='middle'
-              >
-                <Header
-                  content={i18n.t('ListFilters.title')}
-                />
+            <Grid columns={2}>
+              <Grid.Column verticalAlign="middle">
+                <Header content={i18n.t("ListFilters.title")} />
               </Grid.Column>
-              <Grid.Column
-                textAlign='right'
-              >
+              <Grid.Column textAlign="right">
                 <DropdownButton
-                  color='green'
-                  icon='plus'
+                  color="green"
+                  icon="plus"
                   options={_.map(filters, (filter) => ({
                     key: filter.key,
                     value: filter.key,
-                    text: filter.label
+                    text: filter.label,
                   }))}
                   onChange={(e, { value }) => {
                     const filter = _.findWhere(props.filters, { key: value });
-                    props.onSaveChildAssociation('filters', props.onCreateFilter({
-                      ...filter,
-                      operator: FilterOperators.equal
-                    }));
+                    props.onSaveChildAssociation(
+                      "filters",
+                      props.onCreateFilter({
+                        ...filter,
+                        operator: FilterOperators.equal,
+                      })
+                    );
                   }}
                   scrolling
-                  text={i18n.t('ListFilters.buttons.add')}
-                  value=''
+                  text={i18n.t("ListFilters.buttons.add")}
+                  value=""
                 />
                 <Button
-                  color='red'
-                  content={i18n.t('ListFilters.buttons.reset')}
-                  icon='repeat'
+                  color="red"
+                  content={i18n.t("ListFilters.buttons.reset")}
+                  icon="repeat"
                   onClick={() => props.onReset()}
                   style={{
-                    marginLeft: '1em'
+                    marginLeft: "1em",
                   }}
                 />
               </Grid.Column>
             </Grid>
           </Modal.Header>
           <Modal.Content>
-            { !_.isEmpty(props.item.filters) && (
+            {!_.isEmpty(props.item.filters) && (
               <Grid>
-                { _.map(props.item.filters, (filter) => (
-                  <Grid.Row
-                    columns={4}
-                    key={filter.key}
-                    verticalAlign='middle'
-                  >
+                {_.map(props.item.filters, (filter) => (
+                  <Grid.Row columns={4} key={filter.key} verticalAlign="middle">
                     <Grid.Column>
-                      <Header
-                        content={filter.label}
-                      />
+                      <Header content={filter.label} />
                     </Grid.Column>
-                    <Grid.Column
-                      width={5}
-                    >
+                    <Grid.Column width={5}>
                       <Dropdown
                         options={getOperatorsByType(filter.type)}
-                        onChange={(e, { value }) => props.onSaveChildAssociation('filters', {
-                          ..._.omit(filter, 'value'),
-                          operator: value,
-                        })}
+                        onChange={(e, { value }) =>
+                          props.onSaveChildAssociation("filters", {
+                            ..._.omit(filter, "value"),
+                            operator: value,
+                          })
+                        }
                         selection
                         value={filter.operator}
                       />
                     </Grid.Column>
-                    <Grid.Column
-                      width={5}
-                    >
-                      { renderInput(filter) }
-                    </Grid.Column>
-                    <Grid.Column
-                      width={1}
-                    >
+                    <Grid.Column width={5}>{renderInput(filter)}</Grid.Column>
+                    <Grid.Column width={1}>
                       <Button
                         basic
-                        icon='times'
-                        onClick={() => props.onDeleteChildAssociation('filters', filter)}
+                        icon="times"
+                        onClick={() =>
+                          props.onDeleteChildAssociation("filters", filter)
+                        }
                       />
                     </Grid.Column>
                   </Grid.Row>
@@ -396,7 +410,7 @@ const ListFilters = (props: Props) => {
               </Grid>
             )}
           </Modal.Content>
-          { props.children }
+          {props.children}
         </Modal>
       )}
     </ModalContext.Consumer>
@@ -405,12 +419,6 @@ const ListFilters = (props: Props) => {
 
 export default ListFilters;
 
-export {
-  FilterTypes,
-  FilterOperators,
-  FilterOperatorOptions
-};
+export { FilterTypes, FilterOperators, FilterOperatorOptions };
 
-export type {
-  Filter
-};
+export type { Filter };
