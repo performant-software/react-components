891be6945bebe9d718cf96536d4b51a9d090a462
camdendotlol <c@camdenmecklem.com>
2024-05-21 20:16:22 +0000

    Deploying to gh-pages from @ performant-software/react-components@d96117c899ff3e6ae62599063cffeda2080b6762 🚀

---
+++ docs/assets/RelatedTaxonomies.stories-CCP2i-r9.js
@@ -0,0 +1,18 @@
+import{j as n}from"./jsx-runtime-D9TvR9R2.js";import"./index-C0dDX-lL.js";import{_ as L}from"./index-default-C_maRo4o.js";import{R as i}from"./RelatedTaxonomies-DlPEHFWG.js";import{u as c}from"./CoreData-D7TwIKTJ.js";import{w as m}from"./CoreDataContextProvider-D6qzoPqZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./RelatedList-CLPx3q7Z.js";import"./clsx-B-dksMZM.js";import"./LoadAnimation-b0qjd8WL.js";import"./createLucideIcon-Cly0WHBx.js";const k={title:"Components/Core Data/RelatedTaxonomies",component:i},r=m(()=>{const o=c();return n.jsx(i,{onLoad:e=>o.fetchRelatedTaxonomies("1",e)})}),a=m(()=>{const o=c();return n.jsx(i,{onLoad:e=>o.fetchRelatedTaxonomies("1",e),renderItem:e=>n.jsx("p",{style:{color:"pink"},children:e.name})})}),t=m(()=>n.jsx(i,{emptyMessage:"No related taxonomies",onLoad:()=>Promise.resolve()})),s=m(()=>{const o=c();return n.jsx(i,{moreLabel:"Load More",onLoad:e=>o.fetchRelatedTaxonomies("1",e).then(d=>(L.extend(d.list,{count:22,pages:5}),d))})});r.__docgenInfo={description:"",methods:[],displayName:"Default"};a.__docgenInfo={description:"",methods:[],displayName:"CustomRender"};t.__docgenInfo={description:"",methods:[],displayName:"EmptyList"};s.__docgenInfo={description:"",methods:[],displayName:"Pagination"};var p,l,u;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`withCoreDataContextProvider(() => {
+  const PlacesService = usePlacesService();
+  return <RelatedTaxonomies onLoad={params => PlacesService.fetchRelatedTaxonomies('1', params)} />;
+})`,...(u=(l=r.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var x,P,v;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`withCoreDataContextProvider(() => {
+  const PlacesService = usePlacesService();
+  return <RelatedTaxonomies onLoad={params => PlacesService.fetchRelatedTaxonomies('1', params)} renderItem={item => <p style={{
+    color: 'pink'
+  }}>{item.name}</p>} />;
+})`,...(v=(P=a.parameters)==null?void 0:P.docs)==null?void 0:v.source}}};var f,h,R;t.parameters={...t.parameters,docs:{...(f=t.parameters)==null?void 0:f.docs,source:{originalSource:"withCoreDataContextProvider(() => <RelatedTaxonomies emptyMessage='No related taxonomies' onLoad={() => Promise.resolve()} />)",...(R=(h=t.parameters)==null?void 0:h.docs)==null?void 0:R.source}}};var S,g,C;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`withCoreDataContextProvider(() => {
+  const PlacesService = usePlacesService();
+  return <RelatedTaxonomies moreLabel='Load More' onLoad={params => PlacesService.fetchRelatedTaxonomies('1', params).then(response => {
+    _.extend(response.list, {
+      count: 22,
+      pages: 5
+    });
+    return response;
+  })} />;
+})`,...(C=(g=s.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};const O=["Default","CustomRender","EmptyList","Pagination"];export{a as CustomRender,r as Default,t as EmptyList,s as Pagination,O as __namedExportsOrder,k as default};
