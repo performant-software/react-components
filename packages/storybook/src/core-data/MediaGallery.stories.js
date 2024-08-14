d89573520bde49496b5fa2f832128398af746dfa
dleadbetter <derek@performantsoftware.com>
2024-08-14 09:59:36 -0400

    RC #291 - Removing z-index from Modal overlay

--- packages/storybook/src/core-data/MediaGallery.stories.js
+++ packages/storybook/src/core-data/MediaGallery.stories.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import MediaGallery from '../../../core-data/src/components/MediaGallery';
+import Modal from '../../../core-data/src/components/Modal';
 
 export default {
   title: 'Components/Core Data/MediaGallery',
@@ -33,5 +34,37 @@ export const Default = () => {
         />
       )}
     </>
+  );
+};
+
+export const InModal = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Modal
+      description='Testing Media Gallery within a Modal'
+      title='Testing Modal'
+      open
+    >
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        <button
+          onClick={() => setOpen(true)}
+          type='button'
+        >
+          Open  Gallery
+        </button>
+      </div>
+      { open && (
+        <MediaGallery
+          manifestUrl='/core_data/public/v1/places/1/manifests/1'
+          onClose={() => setOpen(false)}
+        />
+      )}
+    </Modal>
   );
 };
