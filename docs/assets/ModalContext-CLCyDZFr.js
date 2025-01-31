b21b5584b5ff9634dd6479c5c7639aaccb1f0fb9
dleadbetter <20641961+dleadbetter@users.noreply.github.com>
2025-01-31 20:45:20 +0000

    Deploying to gh-pages from @ performant-software/react-components@b22e4cdc673a71ad83c7f1a5e110f85f7e1d59a4 🚀

--- docs/assets/ModalContext-CLCyDZFr.js
+++
@@ -1,1 +0,0 @@
-import{R as t}from"./index-C0dDX-lL.js";const e=t.createContext();export{e as M};
---
+++ docs/assets/RelatedOrganizations.stories-CGh5FCO1.js
@@ -0,0 +1,25 @@
+import{j as e}from"./jsx-runtime-D9TvR9R2.js";import"./index-C0dDX-lL.js";import{_ as C}from"./index-default-C_maRo4o.js";import{R as i}from"./RelatedOrganizations-BKJ3tXAM.js";import{b as d}from"./CoreData-37WkoVSV.js";import{w as c}from"./CoreDataContextProvider-Btcnl27g.js";import"./_commonjsHelpers-BosuxZz1.js";import"./RelatedList-CXViI6JL.js";import"./clsx-B-dksMZM.js";import"./LoadAnimation-b0qjd8WL.js";import"./createLucideIcon-Cly0WHBx.js";const E={title:"Components/Core Data/RelatedOrganizations",component:i},o=c(()=>{const a=d();return e.jsx(i,{onLoad:r=>a.fetchRelatedOrganizations("1",r)})}),t=c(()=>{const a=d();return e.jsx(i,{onLoad:r=>a.fetchRelatedOrganizations("1",r),renderItem:r=>e.jsxs("p",{children:[`${r.name}:`," ",Math.random()>.5?e.jsx("span",{style:{color:"green"},children:"↑"}):e.jsx("span",{style:{color:"red"},children:"↓"}),e.jsx("strong",{children:`$${(Math.random()*100).toPrecision(4)}`})]})})}),s=c(()=>e.jsx(i,{emptyMessage:"No related organizations",onLoad:()=>Promise.resolve()})),n=c(()=>{const a=d();return e.jsx(i,{moreLabel:"Load More",onLoad:r=>a.fetchRelatedOrganizations("1",r).then(m=>(C.extend(m.list,{count:22,pages:5}),m))})});o.__docgenInfo={description:"",methods:[],displayName:"Default"};t.__docgenInfo={description:"",methods:[],displayName:"CustomRender"};s.__docgenInfo={description:"",methods:[],displayName:"EmptyList"};n.__docgenInfo={description:"",methods:[],displayName:"Pagination"};var p,l,u;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`withCoreDataContextProvider(() => {
+  const PlacesService = usePlacesService();
+  return <RelatedOrganizations onLoad={params => PlacesService.fetchRelatedOrganizations('1', params)} />;
+})`,...(u=(l=o.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var g,h,P;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`withCoreDataContextProvider(() => {
+  const PlacesService = usePlacesService();
+  return <RelatedOrganizations onLoad={params => PlacesService.fetchRelatedOrganizations('1', params)} renderItem={item => <p>
+          {\`\${item.name}:\`}
+          &nbsp;
+          {Math.random() > 0.5 ? <span style={{
+      color: 'green'
+    }}>↑</span> : <span style={{
+      color: 'red'
+    }}>↓</span>}
+          <strong>{\`$\${(Math.random() * 100).toPrecision(4)}\`}</strong>
+        </p>} />;
+})`,...(P=(h=t.parameters)==null?void 0:h.docs)==null?void 0:P.source}}};var v,f,x;s.parameters={...s.parameters,docs:{...(v=s.parameters)==null?void 0:v.docs,source:{originalSource:"withCoreDataContextProvider(() => <RelatedOrganizations emptyMessage='No related organizations' onLoad={() => Promise.resolve()} />)",...(x=(f=s.parameters)==null?void 0:f.docs)==null?void 0:x.source}}};var R,S,z;n.parameters={...n.parameters,docs:{...(R=n.parameters)==null?void 0:R.docs,source:{originalSource:`withCoreDataContextProvider(() => {
+  const PlacesService = usePlacesService();
+  return <RelatedOrganizations moreLabel='Load More' onLoad={params => PlacesService.fetchRelatedOrganizations('1', params).then(response => {
+    _.extend(response.list, {
+      count: 22,
+      pages: 5
+    });
+    return response;
+  })} />;
+})`,...(z=(S=n.parameters)==null?void 0:S.docs)==null?void 0:z.source}}};const b=["Default","CustomRender","EmptyList","Pagination"];export{t as CustomRender,o as Default,s as EmptyList,n as Pagination,b as __namedExportsOrder,E as default};
