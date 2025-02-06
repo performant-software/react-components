1ea1bfdcdbb888ca2f277cc21fcb5f69dc1aeb39
Camden <c@camdenmecklem.com>
2025-02-06 10:33:33 -0500

    Core Data Pill component (#340)
    * WIP
    
    WIP
    
    WIP
    
    * cleanup and fixes
    
    * add export
    
    * use our own Close icon

---
+++ packages/core-data/src/components/Pill.js
@@ -0,0 +1,58 @@
+// @flow
+
+import React from 'react';
+import clsx from 'clsx';
+import Icon from '../components/Icon';
+
+type Props = {
+  /**
+   * Text to display inside the pill.
+   */
+  label: string,
+  /**
+   * (Optional) Callback fired when the X button is clicked. The X button will only be rendered if onRemove is provided.
+   * @param
+   */
+  onRemove?: (data) => any,
+  /**
+   * (Optional) Background color for the pill.
+   */
+  primary?: string
+}
+
+const Pill = (props: Props) => (
+  <div
+    className={clsx(
+      'text-white',
+      'fill-white',
+      'inline-flex',
+      'gap-2',
+      'justify-center',
+      'items-center',
+      'rounded-[50px]',
+      'text-sm',
+      'py-1',
+      'pl-2.5',
+      { 'pr-1.5': !!props.onRemove },
+      { 'pr-2.5': !props.onRemove },
+      { 'bg-primary': !props.primary }
+    )}
+    style={{ backgroundColor: props.primary }}
+  >
+    <span>{props.label}</span>
+    {props.onRemove && (
+    <button
+      className='flex justify-center items-center h-full'
+      onClick={props.onRemove}
+      type='button'
+    >
+      <Icon
+        name='close'
+        size={14}
+      />
+    </button>
+    )}
+  </div>
+);
+
+export default Pill;
---
+++ packages/storybook/src/core-data/Pill.stories.js
@@ -0,0 +1,31 @@
+// @flow
+
+import { action } from '@storybook/addon-actions';
+import React from 'react';
+import Pill from '../../../core-data/src/components/Pill';
+
+export default {
+  title: 'Components/Core Data/Pill',
+  component: Pill
+};
+
+export const Default = () => (
+  <Pill
+    label='Chip Text'
+  />
+);
+
+export const OnRemove = () => (
+  <Pill
+    label='Chip Text'
+    onRemove={action('click')}
+  />
+);
+
+export const CustomColor = () => (
+  <Pill
+    label='Chip Text'
+    onRemove={action('click')}
+    primary='#fc94af'
+  />
+);
