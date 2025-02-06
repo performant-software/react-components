1f8c750d4a554f79bc805c015eb8ae1a841504b2
camdendotlol <c@camdenmecklem.com>
2025-02-06 15:38:09 +0000

    Deploying to gh-pages from @ performant-software/react-components@1dd7b0f4e2b7b84427846f70cdba815f4bfe1a58 🚀

---
+++ docs/assets/RelatedItemsList.stories-9fF8du7U.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-BTCK_S7S.js";import"./index-CBqU2yxZ.js";import{R as r,a}from"./RelatedItemsList-BCpFtwrc.js";import{R as p}from"./RelatedList-zhBYhkPd.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-default-C_maRo4o.js";import"./LoadAnimation-B5PEUokg.js";import"./createLucideIcon-CT11eyui.js";import"./extends-CCbyfPlC.js";import"./index-dIAcmska.js";import"./index-DZGZymok.js";import"./index-fz1Ew-zJ.js";import"./index-CUFo5YVF.js";import"./clsx-B-dksMZM.js";import"./CoreData-Ds0I0oFM.js";const D={title:"Components/Core Data/RelatedItemsList",component:r},e=()=>t.jsx(r,{children:t.jsx(a,{id:"numbers",label:"Numbers",children:t.jsx(p,{className:"p-3 pt-1 pb-4",collectionName:"items",onLoad:()=>Promise.resolve({items:["One","Two","Three"]}),renderItem:i=>i})})});e.__docgenInfo={description:"",methods:[],displayName:"Default"};var s,o,m;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`() => <RelatedItemsList>
+    <RelatedItem id='numbers' label='Numbers'>
+      <RelatedList className='p-3 pt-1 pb-4' collectionName='items' onLoad={() => Promise.resolve({
+      items: ['One', 'Two', 'Three']
+    })} renderItem={item => item} />
+    </RelatedItem>
+  </RelatedItemsList>`,...(m=(o=e.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};const T=["Default"];export{e as Default,T as __namedExportsOrder,D as default};
--- docs/assets/RelatedItemsList.stories-DrOiFbwj.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./jsx-runtime-BTCK_S7S.js";import"./index-CBqU2yxZ.js";import{R as r,a}from"./RelatedItemsList-DoYLgn7h.js";import{R as p}from"./RelatedList-zhBYhkPd.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-default-C_maRo4o.js";import"./LoadAnimation-B5PEUokg.js";import"./createLucideIcon-CT11eyui.js";import"./extends-CCbyfPlC.js";import"./index-BtRXy3hf.js";import"./index-DZGZymok.js";import"./index-viPvum23.js";import"./index-D-Qi2jLc.js";import"./clsx-B-dksMZM.js";import"./CoreData-Ds0I0oFM.js";const D={title:"Components/Core Data/RelatedItemsList",component:r},e=()=>t.jsx(r,{children:t.jsx(a,{id:"numbers",label:"Numbers",children:t.jsx(p,{className:"p-3 pt-1 pb-4",collectionName:"items",onLoad:()=>Promise.resolve({items:["One","Two","Three"]}),renderItem:i=>i})})});e.__docgenInfo={description:"",methods:[],displayName:"Default"};var s,o,m;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`() => <RelatedItemsList>
-    <RelatedItem id='numbers' label='Numbers'>
-      <RelatedList className='p-3 pt-1 pb-4' collectionName='items' onLoad={() => Promise.resolve({
-      items: ['One', 'Two', 'Three']
-    })} renderItem={item => item} />
-    </RelatedItem>
-  </RelatedItemsList>`,...(m=(o=e.parameters)==null?void 0:o.docs)==null?void 0:m.source}}};const T=["Default"];export{e as Default,T as __namedExportsOrder,D as default};
