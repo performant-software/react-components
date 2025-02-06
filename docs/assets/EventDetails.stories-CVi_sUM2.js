a3424b6c3e3bbfd970a5d4e7611331f0ce08c8bc
camdendotlol <c@camdenmecklem.com>
2025-02-06 22:32:50 +0000

    Deploying to gh-pages from @ performant-software/react-components@85dffac2d9357c652520d815f3f23c2c089271ba 🚀

---
+++ docs/assets/EventDetails.stories-CVi_sUM2.js
@@ -0,0 +1,42 @@
+import{j as e}from"./jsx-runtime-BTCK_S7S.js";import{f as h}from"./index-xJp9Kd2E.js";import{k as v,X as f,i as x,z as R}from"./peripleo-maplibre.es11-BGQsLm5V.js";import"./index-CBqU2yxZ.js";import"./index-DZGZymok.js";import{a as u}from"./chunk-WFFRPTHA-B2vgThZf.js";import{_ as y}from"./index-default-C_maRo4o.js";import{E as a}from"./EventDetails-CuIj3Zxa.js";import{m as g}from"./MapStyles-Ckvdsjal.js";import{R as j}from"./RelatedSources-CE2-9c7l.js";import{R as E}from"./RelatedMedia-BBDR-IML.js";import{R as N}from"./RelatedPeople-_rjzMLg3.js";import{R as P}from"./RelatedPlacesLayer-CsmBCkp_.js";import{w as c}from"./CoreDataContextProvider-BTUWExMB.js";import{u as D}from"./CoreData-Ds0I0oFM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./preview-errors-CzpZmerH.js";import"./index-CKJfshIT.js";import"./v4-D8aEg3BZ.js";import"./Event-B2K4Apks.js";import"./index.es33-CvRc5KCt.js";import"./iframe-CNlobced.js";import"../sb-preview/runtime.js";import"./LoadAnimation-B5PEUokg.js";import"./index-pX2EBQxa.js";import"./redux-C8f_Y06i.js";import"./objectSpread2-N4c0DWqo.js";import"./defineProperty-CCxoMnQV.js";import"./toPropertyKey-hKuLU48H.js";import"./index-C1oKbrXX.js";import"./extends-CCbyfPlC.js";import"./index-BOVMYYdP.js";import"./index-DRE-Fy6J.js";import"./Combination-Cz09LwNY.js";import"./floating-ui.react-dom-CacSY337.js";import"./index-mmsaDusg.js";import"./component-DZ72w8lP.js";import"./___vite-browser-external_commonjs-proxy-DRaEfepn.js";import"./index-DiU2Igp9.js";import"./MediaGallery-Gk83udXM.js";import"./Modal-BDihfBid.js";import"./clsx-B-dksMZM.js";import"./createLucideIcon-CT11eyui.js";import"./RelatedList-zhBYhkPd.js";import"./index.es11-B44KZLfk.js";import"./index-DnNB6xa5.js";const ue={title:"Components/Core Data/EventDetails",component:a},o=c(()=>e.jsx(a,{id:"1"})),s=c(()=>{const r=D();return e.jsxs("div",{className:"p-3",children:[e.jsx(a,{id:1}),e.jsxs("div",{className:"py-2",children:[e.jsx("h2",{className:"py-1 font-bold text-lg",children:"Event Location"}),e.jsx(v,{children:e.jsxs(f,{style:g,children:[e.jsx(x,{position:"topright",children:e.jsx(R,{})}),e.jsx("div",{style:{width:"100%",height:"300px"},children:e.jsx(P,{buffer:10,onLoad:t=>r.fetchRelatedPlaces("1",t)})})]})})]}),e.jsxs("div",{className:"py-2",children:[e.jsx("h2",{className:"py-2 font-bold text-lg",children:"Related Resources"}),e.jsx(j,{onClick:t=>u("click")(t),onLoad:t=>r.fetchRelatedItems("1",t),renderDescription:()=>h.date.anytime().toLocaleDateString(),renderHeader:()=>y.sample(["documents","photos","certificates","licenses"]),renderImage:t=>e.jsx("img",{alt:"Test",src:`https://picsum.photos/800/600?random=${t.uuid}`}),sourceType:"items"})]}),e.jsxs("div",{className:"py-2",children:[e.jsx("h2",{className:"py-2 font-bold text-lg",children:"Related People"}),e.jsx(N,{itemsPerRow:2,onLoad:t=>r.fetchRelatedPeople("1",t)})]}),e.jsx("div",{className:"py-2",children:e.jsx("h2",{className:"py-2 font-bold text-lg",children:"Event Images"})}),e.jsx(E,{itemsPerRow:4,onLoad:t=>r.fetchRelatedManifests("1",t)})]})});o.__docgenInfo={description:"",methods:[],displayName:"Default"};s.__docgenInfo={description:"",methods:[],displayName:"RelatedRecords"};var i,n,m;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:"withCoreDataContextProvider(() => <EventDetails id='1' />)",...(m=(n=o.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};var p,d,l;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`withCoreDataContextProvider(() => {
+  const EventsService = useEventsService();
+  return <div className='p-3'>
+      <EventDetails id={1} />
+      <div className='py-2'>
+        <h2 className='py-1 font-bold text-lg'>
+          Event Location
+        </h2>
+        <Peripleo>
+          <Map style={mapStyle}>
+            <Controls position='topright'>
+              <Zoom />
+            </Controls>
+            <div style={{
+            width: '100%',
+            height: '300px'
+          }}>
+              <RelatedPlacesLayer buffer={10} onLoad={params => EventsService.fetchRelatedPlaces('1', params)} />
+            </div>
+          </Map>
+        </Peripleo>
+      </div>
+      <div className='py-2'>
+        <h2 className='py-2 font-bold text-lg'>
+          Related Resources
+        </h2>
+        <RelatedSources onClick={item => action('click')(item)} onLoad={params => EventsService.fetchRelatedItems('1', params)} renderDescription={() => faker.date.anytime().toLocaleDateString()} renderHeader={() => _.sample(['documents', 'photos', 'certificates', 'licenses'])} renderImage={item => <img alt='Test' src={\`https://picsum.photos/800/600?random=\${item.uuid}\`} />} sourceType='items' />
+      </div>
+      <div className='py-2'>
+        <h2 className='py-2 font-bold text-lg'>
+          Related People
+        </h2>
+        <RelatedPeople itemsPerRow={2} onLoad={params => EventsService.fetchRelatedPeople('1', params)} />
+      </div>
+      <div className='py-2'>
+        <h2 className='py-2 font-bold text-lg'>
+          Event Images
+        </h2>
+      </div>
+      <RelatedMedia itemsPerRow={4} onLoad={params => EventsService.fetchRelatedManifests('1', params)} />
+    </div>;
+})`,...(l=(d=s.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const ye=["Default","RelatedRecords"];export{o as Default,s as RelatedRecords,ye as __namedExportsOrder,ue as default};
--- docs/assets/preview-BnzL6PM5.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-BWtE4CjS.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-BHVxTjDT.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-BHVxTjDT.js","./chunk-HLWAVYOI-BlH3S5nY.js","./iframe-BWtE4CjS.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-DnM9deRA.js","./index-DZGZymok.js","./pickBy-ILmh4CwI.js","./mapValues-BXr-GEgA.js","./_commonjs-dynamic-modules-TDtrdbi3.js","./index-Cw1G72V2.js","./extends-CCbyfPlC.js","./assertThisInitialized-B9jnkVVz.js","./inheritsLoose-1HkgTRWY.js","./setPrototypeOf-DgZC2w_0.js","./getPrototypeOf-BYVhAdwF.js","./cloneDeep-s4xp0F9Z.js","./index-CKJfshIT.js","./index-ozjCl_Kl.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
