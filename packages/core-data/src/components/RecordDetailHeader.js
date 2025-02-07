be8c92d772c180e4b27bab27a0bd9f764c547284
Rebecca Black <rebecca.ann.black@gmail.com>
2025-02-07 01:40:14 -0500

    added detail panel header component

---
+++ packages/core-data/src/components/RecordDetailHeader.js
@@ -0,0 +1,42 @@
+// @flow
+
+import clsx from 'clsx';
+import React from 'react';
+import Icon from './Icon';
+
+type Props = {
+  /**
+   * Class name to apply to the header element.
+   */
+  className?: string,
+
+  /**
+   * The icon to display before the header.
+   */
+  icon?: string,
+
+  /**
+   * The text of the header.
+   */
+  text: string,
+};
+
+const RecordDetailHeader = (props: Props) => (
+  <h1
+    className={clsx(
+      'flex',
+      'flex-row',
+      'gap-2',
+      'items-center',
+      'text-[20px]',
+      'font-bold',
+      'leading-[120%]',
+      props.className
+    )}
+  >
+    { props.icon && <Icon name={props.icon} size={24} /> }
+    <span>{ props.text }</span>
+  </h1>
+);
+
+export default RecordDetailHeader;
---
+++ packages/storybook/src/core-data/RecordDetailHeader.stories.js
@@ -0,0 +1,30 @@
+// @flow
+
+import React from 'react';
+import Button from '../../../core-data/src/components/RecordDetailHeader';
+import RecordDetailHeader from '../../../core-data/src/components/RecordDetailHeader';
+
+export default {
+  title: 'Components/Core Data/RecordDetailHeader',
+  component: RecordDetailHeader
+};
+
+export const Default = () => (
+  <RecordDetailHeader
+    text='This is a header!'
+  />
+);
+
+export const Icon = () => (
+  <RecordDetailHeader
+    text='This is a place name!'
+    icon='location'
+  />
+);
+
+export const Multiline = () => (
+  <RecordDetailHeader
+    text='This is a really long header that goes onto the second line'
+    className='w-80'
+  />
+)
