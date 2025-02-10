3028672d11f9f16739d763a306038986d297b4c9
Camden Mecklem <c@camdenmecklem.com>
2025-02-10 16:12:17 -0500

    convert to combobox

--- packages/core-data/src/components/Combobox.js
+++ packages/core-data/src/components/Combobox.js
@@ -3,7 +3,6 @@
 import React, { useCallback, useState } from 'react';
 import * as Popover from '@radix-ui/react-popover';
 import Icon from './Icon';
-import Input from './Input';
 import i18n from '../i18n/i18n';
 import './Combobox.css';
 
@@ -66,11 +65,19 @@ type Props = {
 
 const Combobox = (props: Props) => {
   const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState('');
 
   const isSelected = (option: Option) => useCallback(
     props.values.some((opt) => opt.value === option.value),
     [props.values]
   );
+
+  const onSearchChange = useCallback((val: string) => {
+    setSearch(val);
+    if (props.onSearch) {
+      props.onSearch(val);
+    }
+  }, [props.onSearch]);
 
   return (
     <div className='w-full flex flex-col gap-3 py-4'>
@@ -99,16 +106,16 @@ const Combobox = (props: Props) => {
       >
         <Popover.Trigger asChild>
           <div
-            className='flex items-center justify-center gap-1 pr-2 pl-3 rounded-[5px] bg-white w-full min-h-10'
-            onClick={() => setOpen(!open)}
-            type='button'
+            className='flex items-center justify-center gap-1 pr-2 pl-3 rounded-[5px] bg-white w-full min-h-10 border focus-within:border-primary'
           >
-            <div className='grow flex gap-2 flex-wrap py-2'>
+            <div className='grow flex gap-2 flex-wrap p-2'>
               {props.values.length === 0 && (
-                <Input
-                  
+                <input
+                  className='focus:outline-none   w-full'
+                  placeholder={props.placeholder || i18n.t('Dropdown.select')}
+                  onChange={(e) => onSearchChange(e.target.value)}
+                  value={search}
                 />
-                <span className='grow text-left justify-self-start text-neutral-500'>{props.placeholder || i18n.t('Dropdown.select')}</span>
               )}
               {props.values.map((value) => (
                 <ComboboxValue
@@ -117,7 +124,7 @@ const Combobox = (props: Props) => {
                 />
               ))}
             </div>
-            <div className='flex gap-2'>
+            <div className='flex gap-2 h-full'>
               {props.onClear && props.values.length > 0 && (
                 <button
                   onClick={props.onClear}
@@ -128,9 +135,15 @@ const Combobox = (props: Props) => {
                   />
                 </button>
               )}
-              <Icon
-                name='down'
-              />
+              <button
+                className='flex items-center justify-center h-8 w-8 rounded-full'
+                onClick={() => setOpen(!open)}
+                type='button'
+              >
+                <Icon
+                  name='down'
+                />
+              </button>
             </div>
           </div>
         </Popover.Trigger>
