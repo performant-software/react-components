706e5c6357550b9fb8883b0f4fc7994a3a7ac627
camdendotlol <c@camdenmecklem.com>
2025-02-11 14:02:49 +0000

    Deploying to gh-pages from @ performant-software/react-components@772ce574e3cbea6d4fb67dc54fe860af20d11235 🚀

---
+++ docs/assets/Facet.stories-Dq8W3wt3.js
@@ -0,0 +1,120 @@
+import{j as e}from"./jsx-runtime-BTCK_S7S.js";import{d as m}from"./index-CDLeJpy6.js";import{r as c}from"./index-CBqU2yxZ.js";import{F as n}from"./Form-fErk-qk_.js";import{B as F}from"./Button-Blq7Z6RX.js";import{C as l}from"./Checkbox-C9uAfnsM.js";import{F as i}from"./Facet-C0cvIU-z.js";import"./mapValues-BntjSFt6.js";import"./_commonjsHelpers-BosuxZz1.js";import"./pickBy-CC7CsC_n.js";import"./_commonjs-dynamic-modules-TDtrdbi3.js";import"./isPlainObject-D9-U-Vjl.js";import"./index-Cw1G72V2.js";import"./index-CKJfshIT.js";import"./tiny-invariant.cjs-DxxScMW_.js";import"./___vite-browser-external_commonjs-proxy-DRaEfepn.js";import"./preview-errors-CzpZmerH.js";import"./debounce-BANtZgGn.js";import"./extends-CCbyfPlC.js";import"./inheritsLoose-1HkgTRWY.js";import"./setPrototypeOf-DgZC2w_0.js";import"./getElementType-CckcRzoH.js";import"./Label-Cq1fb1cd.js";import"./ModernAutoControlledComponent-DJBWk5EW.js";import"./assertThisInitialized-B9jnkVVz.js";import"./Icon-2tEtgK8g.js";import"./includes-CQoJOerW.js";import"./toFinite-KQ8KevIC.js";import"./forEach-CXlln0XP.js";import"./Dimmer-D_gwaesu.js";import"./Portal-DvhHJPzU.js";import"./keyboardKey-OMDgGVm3.js";import"./index-DZGZymok.js";import"./objectWithoutPropertiesLoose-1QL-frMM.js";import"./Dropdown-Di2cPjwR.js";import"./map-B4PnIEPz.js";import"./without-BP-mqg2K.js";import"./isArrayLikeObject-CbXKTyfk.js";import"./isEmpty-Cyb9hIKi.js";import"./deburr-CEfyoYOr.js";import"./index-BBpNyw-T.js";import"./_baseSet-6y0qliU9.js";import"./_baseAssignValue-izmKBXUH.js";import"./_arrayReduce-CrxnWFSq.js";import"./filter-CVNi8iPS.js";import"./Input--hQVCRQV.js";import"./Divider-C9xp3Rj7.js";import"./Header-BJ3NNdPz.js";import"./Accordion-hxhfwUID.js";const De={title:"Components/Semantic UI/Facet",component:i},t=()=>e.jsx(i,{title:"Department",children:e.jsxs(n,{children:[e.jsx(n.Field,{children:e.jsx(l,{label:"Appliances"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Bath & Faucets"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Building Materials"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Doors & Windows"})})]})}),a=()=>e.jsx(i,{defaultActive:!1,title:"Department",children:e.jsxs(n,{children:[e.jsx(n.Field,{children:e.jsx(l,{label:"Appliances"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Bath & Faucets"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Building Materials"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Doors & Windows"})})]})}),s=()=>e.jsxs(e.Fragment,{children:[e.jsx(i,{divided:m.boolean("Divided",!0),title:"Department",children:e.jsxs(n,{children:[e.jsx(n.Field,{children:e.jsx(l,{label:"Appliances"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Bath & Faucets"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Building Materials"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Doors & Windows"})})]})}),e.jsx(i,{divided:m.boolean("Divided",!0),title:"Brand",children:e.jsxs(n,{children:[e.jsx(n.Field,{children:e.jsx(l,{label:"American Standard"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Andersen"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Delta"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Kohler"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"LG"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Samsung"})})]})})]}),d=()=>{const r=c.useRef(),v=c.useCallback(()=>{const{current:o}=r;o&&o.collapse()},[r.current]),A=c.useCallback(()=>{const{current:o}=r;o&&o.expand()},[r.current]);return e.jsxs(e.Fragment,{children:[e.jsx(i,{innerRef:r,title:"Department",children:e.jsxs(n,{children:[e.jsx(n.Field,{children:e.jsx(l,{label:"Appliances"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Bath & Faucets"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Building Materials"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Doors & Windows"})})]})}),e.jsx(F,{content:"Expand",onClick:A}),e.jsx(F,{content:"Collapse",onClick:v})]})};t.__docgenInfo={description:"",methods:[],displayName:"Default"};a.__docgenInfo={description:"",methods:[],displayName:"Collapsed"};s.__docgenInfo={description:"",methods:[],displayName:"Divided"};d.__docgenInfo={description:"",methods:[],displayName:"ExpandCollapse"};var p,x,b;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`() => <Facet title='Department'>
+    <Form>
+      <Form.Field>
+        <Checkbox label='Appliances' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Bath & Faucets' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Building Materials' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Doors & Windows' />
+      </Form.Field>
+    </Form>
+  </Facet>`,...(b=(x=t.parameters)==null?void 0:x.docs)==null?void 0:b.source}}};var h,u,j;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`() => <Facet defaultActive={false} title='Department'>
+    <Form>
+      <Form.Field>
+        <Checkbox label='Appliances' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Bath & Faucets' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Building Materials' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Doors & Windows' />
+      </Form.Field>
+    </Form>
+  </Facet>`,...(j=(u=a.parameters)==null?void 0:u.docs)==null?void 0:j.source}}};var C,f,k;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`() => <>
+    <Facet divided={boolean('Divided', true)} title='Department'>
+      <Form>
+        <Form.Field>
+          <Checkbox label='Appliances' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Bath & Faucets' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Building Materials' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Doors & Windows' />
+        </Form.Field>
+      </Form>
+    </Facet>
+    <Facet divided={boolean('Divided', true)} title='Brand'>
+      <Form>
+        <Form.Field>
+          <Checkbox label='American Standard' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Andersen' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Delta' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Kohler' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='LG' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Samsung' />
+        </Form.Field>
+      </Form>
+    </Facet>
+  </>`,...(k=(f=s.parameters)==null?void 0:f.docs)==null?void 0:k.source}}};var D,B,g;d.parameters={...d.parameters,docs:{...(D=d.parameters)==null?void 0:D.docs,source:{originalSource:`() => {
+  const ref = useRef();
+
+  /**
+   * Collapses the facet via the imperative function.
+   *
+   * @type {(function(): void)|*}
+   */
+  const onCollapse = useCallback(() => {
+    const {
+      current: instance
+    } = ref;
+    if (instance) {
+      instance.collapse();
+    }
+  }, [ref.current]);
+
+  /**
+   * Expands the facet via the imperative function.
+   *
+   * @type {(function(): void)|*}
+   */
+  const onExpand = useCallback(() => {
+    const {
+      current: instance
+    } = ref;
+    if (instance) {
+      instance.expand();
+    }
+  }, [ref.current]);
+  return <>
+      <Facet innerRef={ref} title='Department'>
+        <Form>
+          <Form.Field>
+            <Checkbox label='Appliances' />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label='Bath & Faucets' />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label='Building Materials' />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label='Doors & Windows' />
+          </Form.Field>
+        </Form>
+      </Facet>
+      <Button content='Expand' onClick={onExpand} />
+      <Button content='Collapse' onClick={onCollapse} />
+    </>;
+}`,...(g=(B=d.parameters)==null?void 0:B.docs)==null?void 0:g.source}}};const Be=["Default","Collapsed","Divided","ExpandCollapse"];export{a as Collapsed,t as Default,s as Divided,d as ExpandCollapse,Be as __namedExportsOrder,De as default};
---
+++ docs/assets/LazyImage.stories-CyPNDhwv.js
@@ -0,0 +1,63 @@
+import{j as e}from"./jsx-runtime-BTCK_S7S.js";import"./index-CBqU2yxZ.js";import{a as h}from"./chunk-WFFRPTHA-B2vgThZf.js";import{B as u}from"./Button-Blq7Z6RX.js";import{C as o}from"./Card-BJT3uTAq.js";import{I as a}from"./Item-9lJy5i0K.js";import{i as t}from"./test-image-BGrKnxUR.js";import{L as r}from"./LazyImage-CUZp6xZi.js";import{p as M}from"./portrait-test-image-CFX7vfGN.js";import"./_commonjsHelpers-BosuxZz1.js";import"./preview-errors-CzpZmerH.js";import"./index-CKJfshIT.js";import"./v4-D8aEg3BZ.js";import"./extends-CCbyfPlC.js";import"./inheritsLoose-1HkgTRWY.js";import"./setPrototypeOf-DgZC2w_0.js";import"./getElementType-CckcRzoH.js";import"./Icon-2tEtgK8g.js";import"./Label-Cq1fb1cd.js";import"./ModernAutoControlledComponent-DJBWk5EW.js";import"./assertThisInitialized-B9jnkVVz.js";import"./includes-CQoJOerW.js";import"./toFinite-KQ8KevIC.js";import"./forEach-CXlln0XP.js";import"./Dimmer-D_gwaesu.js";import"./Portal-DvhHJPzU.js";import"./keyboardKey-OMDgGVm3.js";import"./index-DZGZymok.js";import"./objectWithoutPropertiesLoose-1QL-frMM.js";import"./map-B4PnIEPz.js";import"./LazyLoader-D1S4_28m.js";import"./without-BP-mqg2K.js";import"./isArrayLikeObject-CbXKTyfk.js";import"./Loader-CooidObp.js";import"./Segment-C5_maSSM.js";import"./Transition-DSFEXhCu.js";import"./_baseAssignValue-izmKBXUH.js";import"./filter-CVNi8iPS.js";import"./i18n-DOVQ27W-.js";import"./i18next-Sc0HEOoc.js";import"./toPropertyKey-hKuLU48H.js";import"./defineProperty-CCxoMnQV.js";import"./inherits-D5wXBZoG.js";import"./createClass-D2z1h_eG.js";import"./getPrototypeOf-BYVhAdwF.js";import"./DownloadButton-BvcDWdH9.js";import"./uuid-BH6G0oTz.js";import"./PhotoViewer-CZNm3XYc.js";import"./index.es33-DaapAGZu.js";import"./iframe-CanRUTyQ.js";import"../sb-preview/runtime.js";import"./Message-3p6wyYpA.js";import"./Modal-Bz2VEyQG.js";import"./index-BBpNyw-T.js";import"./_baseSet-6y0qliU9.js";import"./_arrayReduce-CrxnWFSq.js";import"./isEmpty-Cyb9hIKi.js";const Ye={title:"Components/Semantic UI/LazyImage"},m=()=>e.jsx(r,{image:{alt:"Image"},src:t}),s=()=>e.jsx(r,{}),i=()=>e.jsxs(r,{image:{alt:"Test Image"},src:t,children:[e.jsx(u,{color:"green",content:"Edit photo",icon:"edit",onClick:h("edit")}),e.jsx(u,{color:"orange",content:"Change photo",icon:"move",onClick:h("change")}),e.jsx(u,{color:"red",content:"Delete photo",icon:"trash",onClick:h("delete")})]}),n=()=>e.jsxs(o.Group,{children:[e.jsxs(o,{children:[e.jsx(r,{image:{alt:"Test Image"},src:t}),e.jsx(o.Content,{header:"Test 1"})]}),e.jsxs(o,{children:[e.jsx(r,{image:{alt:"Portrait Image"},src:M}),e.jsx(o.Content,{header:"Test 2"})]}),e.jsxs(o,{children:[e.jsx(r,{image:{alt:"Last Image"}}),e.jsx(o.Content,{header:"Test 3"})]})]}),c=()=>e.jsxs(a.Group,{children:[e.jsxs(a,{children:[e.jsx(a.Image,{children:e.jsx(r,{image:{alt:"First Image"},src:t})}),e.jsx(a.Content,{header:"Test 1"})]}),e.jsxs(a,{children:[e.jsx(a.Image,{children:e.jsx(r,{image:{alt:"Portrait Image"},src:M})}),e.jsx(a.Content,{image:{alt:"Last Image"},header:"Test 2"})]}),e.jsxs(a,{children:[e.jsx(a.Image,{children:e.jsx(r,{})}),e.jsx(a.Content,{header:"Test 3"})]})]}),p=()=>e.jsx(r,{image:{alt:"Image"},src:"path/to/image.jpg"}),g=()=>e.jsx(r,{image:{alt:"Image"},preview:t,src:"path/to/image.jpg"}),d=()=>e.jsx(r,{image:{alt:"Image"},preview:"path/to/image.jpg",src:t}),l=()=>e.jsx(r,{image:{alt:"Image"},preview:"path/to/image.jpg",src:"path/to/image.jpg"}),I=()=>e.jsx(r,{downloadUrl:t,name:"Image.jpg",image:{alt:"Image"},src:t});m.__docgenInfo={description:"",methods:[],displayName:"Default"};s.__docgenInfo={description:"",methods:[],displayName:"Placeholder"};i.__docgenInfo={description:"",methods:[],displayName:"ExtraButtons"};n.__docgenInfo={description:"",methods:[],displayName:"CardContent"};c.__docgenInfo={description:"",methods:[],displayName:"ListContent"};p.__docgenInfo={description:"",methods:[],displayName:"ErrorSource"};g.__docgenInfo={description:"",methods:[],displayName:"ErrorSourceWithPreview"};d.__docgenInfo={description:"",methods:[],displayName:"ErrorPreview"};l.__docgenInfo={description:"",methods:[],displayName:"ErrorPreviewAndSource"};I.__docgenInfo={description:"",methods:[],displayName:"Downloadable"};var j,C,x;m.parameters={...m.parameters,docs:{...(j=m.parameters)==null?void 0:j.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Image'
+}} src={image} />`,...(x=(C=m.parameters)==null?void 0:C.docs)==null?void 0:x.source}}};var y,L,_;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:"() => <LazyImage />",...(_=(L=s.parameters)==null?void 0:L.docs)==null?void 0:_.source}}};var f,w,S;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Test Image'
+}} src={image}>
+    <Button color='green' content='Edit photo' icon='edit' onClick={action('edit')} />
+    <Button color='orange' content='Change photo' icon='move' onClick={action('change')} />
+    <Button color='red' content='Delete photo' icon='trash' onClick={action('delete')} />
+  </LazyImage>`,...(S=(w=i.parameters)==null?void 0:w.docs)==null?void 0:S.source}}};var E,z,v;n.parameters={...n.parameters,docs:{...(E=n.parameters)==null?void 0:E.docs,source:{originalSource:`() => <Card.Group>
+    <Card>
+      <LazyImage image={{
+      alt: 'Test Image'
+    }} src={image} />
+      <Card.Content header='Test 1' />
+    </Card>
+    <Card>
+      <LazyImage image={{
+      alt: 'Portrait Image'
+    }} src={portraitImage} />
+      <Card.Content header='Test 2' />
+    </Card>
+    <Card>
+      <LazyImage image={{
+      alt: 'Last Image'
+    }} />
+      <Card.Content header='Test 3' />
+    </Card>
+  </Card.Group>`,...(v=(z=n.parameters)==null?void 0:z.docs)==null?void 0:v.source}}};var P,T,N;c.parameters={...c.parameters,docs:{...(P=c.parameters)==null?void 0:P.docs,source:{originalSource:`() => <Item.Group>
+    <Item>
+      <Item.Image>
+        <LazyImage image={{
+        alt: 'First Image'
+      }} src={image} />
+      </Item.Image>
+      <Item.Content header='Test 1' />
+    </Item>
+    <Item>
+      <Item.Image>
+        <LazyImage image={{
+        alt: 'Portrait Image'
+      }} src={portraitImage} />
+      </Item.Image>
+      <Item.Content image={{
+      alt: 'Last Image'
+    }} header='Test 2' />
+    </Item>
+    <Item>
+      <Item.Image>
+        <LazyImage />
+      </Item.Image>
+      <Item.Content header='Test 3' />
+    </Item>
+  </Item.Group>`,...(N=(T=c.parameters)==null?void 0:T.docs)==null?void 0:N.source}}};var B,D,k;p.parameters={...p.parameters,docs:{...(B=p.parameters)==null?void 0:B.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Image'
+}} src='path/to/image.jpg' />`,...(k=(D=p.parameters)==null?void 0:D.docs)==null?void 0:k.source}}};var G,b,A;g.parameters={...g.parameters,docs:{...(G=g.parameters)==null?void 0:G.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Image'
+}} preview={image} src='path/to/image.jpg' />`,...(A=(b=g.parameters)==null?void 0:b.docs)==null?void 0:A.source}}};var U,W,F;d.parameters={...d.parameters,docs:{...(U=d.parameters)==null?void 0:U.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Image'
+}} preview='path/to/image.jpg' src={image} />`,...(F=(W=d.parameters)==null?void 0:W.docs)==null?void 0:F.source}}};var O,R,q;l.parameters={...l.parameters,docs:{...(O=l.parameters)==null?void 0:O.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Image'
+}} preview='path/to/image.jpg' src='path/to/image.jpg' />`,...(q=(R=l.parameters)==null?void 0:R.docs)==null?void 0:q.source}}};var H,J,K;I.parameters={...I.parameters,docs:{...(H=I.parameters)==null?void 0:H.docs,source:{originalSource:`() => <LazyImage downloadUrl={image} name='Image.jpg' image={{
+  alt: 'Image'
+}} src={image} />`,...(K=(J=I.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};const Ze=["Default","Placeholder","ExtraButtons","CardContent","ListContent","ErrorSource","ErrorSourceWithPreview","ErrorPreview","ErrorPreviewAndSource","Downloadable"];export{n as CardContent,m as Default,I as Downloadable,d as ErrorPreview,l as ErrorPreviewAndSource,p as ErrorSource,g as ErrorSourceWithPreview,i as ExtraButtons,c as ListContent,s as Placeholder,Ze as __namedExportsOrder,Ye as default};
