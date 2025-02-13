f607626146b58819ab19101a6ece89c9dbe2ab61
Camden <c@camdenmecklem.com>
2025-02-13 15:54:42 -0500

    replace checkbox with SVGs (#381)

--- packages/core-data/src/components/Checkbox.js
+++ packages/core-data/src/components/Checkbox.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import * as RadixCheckbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx';
-import { CheckIcon } from '@radix-ui/react-icons';
+import Icon from './Icon';
 
 type Props = {
   /**
@@ -25,33 +25,19 @@ type Props = {
 const Checkbox = (props: Props) => (
   <RadixCheckbox.Root
     checked={props.checked}
-    className={clsx(
-      '!border-solid',
-      'border',
-      'h-4',
-      'w-4',
-      'rounded-sm',
-      'flex',
-      'items-center',
-      'justify-center',
-      { 'bg-primary': props.checked },
-      { 'hover:bg-primary': props.checked },
-      { 'bg-white': !props.checked },
-      { 'hover:bg-white': !props.checked },
-      { 'border-primary': props.checked },
-      { 'border-black': !props.checked }
-    )}
+    className='rounded-sm'
     disabled={props.disabled}
     onCheckedChange={props.onClick}
   >
-    <RadixCheckbox.Indicator
-      className='w-full h-full'
-    >
-      {props.checked && (
-        <CheckIcon
-          className='h-3.5 w-3.5 text-white'
-        />
-      )}
+    <RadixCheckbox.Indicator asChild forceMount>
+      <Icon
+        className={clsx(
+          { 'fill-primary': props.checked },
+          { 'fill-black': !props.checked },
+          { 'hover:bg-white': !props.checked },
+        )}
+        name={props.checked ? 'checkbox-filled' : 'checkbox'}
+      />
     </RadixCheckbox.Indicator>
   </RadixCheckbox.Root>
 );
