73ade6718bf16edbafc87a6210522943dac1c243
Camden Mecklem <c@camdenmecklem.com>
2025-02-13 15:21:52 -0500

    event handlers, etc. for search list

--- packages/core-data/src/components/SearchListItem.js
+++ packages/core-data/src/components/SearchListItem.js
@@ -1,47 +1,94 @@
 // @flow
 
-import React from 'react';
+import React, { type Element } from 'react';
 import Icon from './Icon';
 import { type Attribute } from '../types/SearchList';
 
-type Props = {
-  /**
-   * Title of the record
-   */
-  title: string,
+type SearchListItemProps = {
   /**
    * Attributes to display (only the first three are shown)
    */
-  attributes: Attribute[],
+  attributes?: Attribute[],
   /**
    * Search item object
    */
-  item: any
+  item: any,
+  /**
+   * Callback that fires when an item is clicked
+   */
+  onClick: (item: any) => void,
+  /**
+   * Callback that fires when the mouse begins to hover over an item
+   */
+  onMouseEnter: (item: any) => void,
+  /**
+   * Callback that fires when the mouse stops hovering over an item
+   */
+  onMouseLeave: (item: any) => void,
+  /**
+   * Title of the record
+   */
+  title: string
 };
 
-const ListItem = (props: Props) => (
-  <li className='py-3 px-6'>
-    <p className='font-bold text-neutral-800'>{props.title}</p>
-    {props.attributes.length > 0 && (
-      <ul className='list-none'>
-        {props.attributes.slice(0, 3).map((att) => (
-          <li
-            className='text-sm text-neutral-800 flex gap-2 items-center list-none pl-5 pt-1'
-            key={att.name}
-          >
-            <Icon
-              className='min-w-[13px]'
-              name={att.icon || 'bullet'}
-              size={13}
-            />
-            {att.render
-              ? att.render(props.item)
-              : props.item[att.name]}
-          </li>
-        ))}
-      </ul>
-    )}
-  </li>
+type ItemWrapperProps = {
+  children: Element<any>,
+  ...Props
+}
+
+/**
+ * Wraps the list item in a button element if an onClick
+ * callback is provided.
+ */
+const ItemWrapper = (props: ItemWrapperProps) => {
+  if (props.onClick) {
+    return (
+      <button
+        className='w-full border-0 text-left'
+        onClick={() => props.onClick(props.item)}
+        type='button'
+      >
+        {props.children}
+      </button>
+    );
+  }
+
+  return props.children;
+};
+
+const SearchListItem = (props: SearchListItemProps) => (
+  <ItemWrapper {...props}>
+    <li
+      className='py-3 px-6'
+      onMouseEnter={props.onMouseEnter
+        ? (item) => props.onMouseEnter(item)
+        : undefined}
+      onMouseLeave={props.onMouseLeave
+        ? (item) => props.onMouseLeave(item)
+        : undefined}
+    >
+      <p className='font-bold text-neutral-800'>{props.title}</p>
+      {props.attributes && props.attributes.length > 0 && (
+        <ul className='list-none'>
+          {props.attributes.slice(0, 3).map((att) => (
+            <li
+              className='text-sm text-neutral-800 flex gap-2 items-center list-none pl-5 pt-1'
+              key={att.name}
+            >
+              <Icon
+                className='min-w-[13px]'
+                name={att.icon || 'bullet'}
+                size={13}
+              />
+              {att.render
+                ? att.render(props.item)
+                : props.item[att.name]}
+            </li>
+          ))}
+        </ul>
+      )}
+    </li>
+  </ItemWrapper>
 );
 
-export default ListItem;
+export default SearchListItem;
--- packages/storybook/src/core-data/SearchList.stories.js
+++ packages/storybook/src/core-data/SearchList.stories.js
@@ -1,21 +1,12 @@
 // @flow
 
+import { action } from '@storybook/addon-actions';
 import React from 'react';
 import SearchList from '../../../core-data/src/components/SearchList';
 import data from '../data/typesense/Places.json';
+import { shuffle } from '../utils/Array';
 
-const LOTS_OF_DATA = [
-  ...data,
-  ...data,
-  ...data,
-  ...data,
-  ...data,
-  ...data,
-  ...data,
-  ...data,
-  ...data,
-  ...data
-];
+const LOTS_OF_DATA = shuffle(new Array(10).fill(data).flat());
 
 LOTS_OF_DATA[0].name = "I'm a really long name to showcase how this component handles really long names.";
 
@@ -76,6 +67,104 @@ export const TitleCallback = () => (
       ]}
       items={LOTS_OF_DATA}
       itemTitle={(item) => `This record's name is "${item.name}"`}
+    />
+  </div>
+);
+
+export const OneResult = () => (
+  <div className='h-[600px] w-[360px]'>
+    <SearchList
+      attributes={[
+        {
+          label: 'UUID',
+          name: 'uuid',
+        },
+        {
+          label: 'Record ID',
+          name: 'record_id',
+          icon: 'person'
+        },
+        {
+          label: 'Location',
+          name: 'geometry',
+          icon: 'location',
+          render: (item) => (item.coordinates
+            ? `${item.coordinates[0]}, ${item.coordinates[1]}`
+            : '')
+        },
+      ]}
+      items={LOTS_OF_DATA.slice(0, 1)}
+      itemTitle='name'
+    />
+  </div>
+);
+
+export const NoAttributes = () => (
+  <div className='h-[600px] w-[360px]'>
+    <SearchList
+      items={LOTS_OF_DATA}
+      itemTitle='name'
+    />
+  </div>
+);
+
+export const EventHandlers = () => (
+  <div className='h-[600px] w-[360px]'>
+    <SearchList
+      attributes={[
+        {
+          label: 'UUID',
+          name: 'uuid',
+        },
+        {
+          label: 'Record ID',
+          name: 'record_id',
+          icon: 'person'
+        },
+        {
+          label: 'Location',
+          name: 'geometry',
+          icon: 'location',
+          render: (item) => (item.coordinates
+            ? `${item.coordinates[0]}, ${item.coordinates[1]}`
+            : '')
+        },
+      ]}
+      items={LOTS_OF_DATA}
+      itemTitle='name'
+      // eslint-disable-next-line no-alert
+      onItemClick={(item) => window.alert(`You clicked on ${item.name}!`)}
+      onItemMouseEnter={action('onMouseEnter')}
+      onItemMouseLeave={action('onMouseLeave')}
+    />
+  </div>
+);
+
+export const CustomStyles = () => (
+  <div className='h-[600px] w-[360px]'>
+    <SearchList
+      attributes={[
+        {
+          label: 'UUID',
+          name: 'uuid',
+        },
+        {
+          label: 'Record ID',
+          name: 'record_id',
+          icon: 'person'
+        },
+        {
+          label: 'Location',
+          name: 'geometry',
+          icon: 'location',
+          render: (item) => (item.coordinates
+            ? `${item.coordinates[0]}, ${item.coordinates[1]}`
+            : '')
+        },
+      ]}
+      className='bg-red-100 border-2 border-red-900 rounded-md'
+      items={LOTS_OF_DATA}
+      itemTitle='name'
     />
   </div>
 );
