6e048f528e47b69ac935c0e82de26e8a2413134c
camdendotlol <c@camdenmecklem.com>
2025-02-17 16:45:31 +0000

    Deploying to gh-pages from @ performant-software/react-components@d632c34fe9510031ae8860599055c9832d2a6e60 🚀

---
+++ docs/assets/RecordDetailPanel.stories-BQWFSc9A.js
@@ -0,0 +1,172 @@
+import{j as e}from"./jsx-runtime-D9TvR9R2.js";import"./index-C0dDX-lL.js";import{c}from"./clsx-B-dksMZM.js";import{I as M}from"./Icon-BwuB1aie.js";import{A as O}from"./AccordionItemsList-nnlvhhOr.js";import{R as G}from"./RecordDetailHeader-hmSN6nU0.js";import{R as E}from"./RecordDetailBreadcrumbs-RtXnXylu.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BGIGw4h8.js";import"./extends-CCbyfPlC.js";import"./index-CIyLsbPd.js";import"./index-DOYXiIEK.js";import"./index-BmSdlTVd.js";import"./index-CMZE9jPC.js";import"./index-default-C_maRo4o.js";import"./Button-DWHfM6Lf.js";import"./RecordDetailTitle-j9jOdXSj.js";import"./RecordDetailItem-UU3-TIye.js";import"./i18n-BYrjldnS.js";import"./i18next-CwS9wMM1.js";import"./toPropertyKey-BcUJYDQU.js";import"./defineProperty-DcNpp85p.js";import"./inherits-CWPG5jll.js";import"./assertThisInitialized-B9jnkVVz.js";import"./setPrototypeOf-DgZC2w_0.js";import"./createClass-CV9nCQMI.js";import"./getPrototypeOf-BYVhAdwF.js";const B=t=>{var u,p,f,v,h;return e.jsxs("div",{className:c("relative","shadow-[0px_5px_12px_0px_rgba(0,0,0,.10)]","overflow-y-auto",(u=t.classNames)==null?void 0:u.root),children:[e.jsxs("div",{className:"sticky inset-0 shadow-[0px_1px_4px_0px_rgba(0,0,0,.15)] bg-white z-[5]",children:[t.onClose&&e.jsx("div",{onClick:t.onClose,onKeyDown:t.onClose,tabIndex:"0",role:"button","aria-label":"Close",className:"absolute top-6 right-6 z-10 cursor-pointer",children:e.jsx(M,{name:"close",size:24})}),(t.breadcrumbs||t.onGoBack)&&e.jsx(E,{history:t.breadcrumbs||[],onGoBack:t.onGoBack,className:"absolute top-6 left-6 pr-6 max-w-[calc(100%_-4.5em)] z-10"}),e.jsx(G,{title:t.title,icon:t.icon,classNames:{root:c({"!pt-16":t.breadcrumbs||t.onGoBack},(p=t.classNames)==null?void 0:p.header),title:c((f=t.classNames)==null?void 0:f.title,{"pr-6":t.onClose}),items:(v=t.classNames)==null?void 0:v.items},detailItems:t.detailItems,detailPageUrl:t.detailPageUrl,children:t.children})]}),e.jsx(O,{className:c("shadow-[0px_1px_4px_rgba(0,0,0,.15)]",(h=t.classNames)==null?void 0:h.relatedRecords),items:t.relations,count:t.count})]})},i=B;B.__docgenInfo={description:"",methods:[],displayName:"RecordDetailPanel",props:{breadcrumbs:{required:!1,flowType:{name:"Array",elements:[{name:"string"}],raw:"Array<string>"},description:"A list of navigation breadcrumbs to be rendered above the title"},children:{required:!1,flowType:{name:"Node"},description:"Content to be rendered as the blurb"},classNames:{required:!1,flowType:{name:"signature",type:"object",raw:"{ header?: string, items?: string, relatedRecords?: string, root?: string, title?: string }",signature:{properties:[{key:"header",value:{name:"string",required:!1}},{key:"items",value:{name:"string",required:!1}},{key:"relatedRecords",value:{name:"string",required:!1}},{key:"root",value:{name:"string",required:!1}},{key:"title",value:{name:"string",required:!1}}]}},description:`Class names to apply to the root div, the header box, the title element,
+the list element containing the detail items, and the accordion list containing related records`},count:{required:!1,flowType:{name:"Boolean"},description:"If true, the number of related records will be displayed for each type"},detailItems:{required:!1,flowType:{name:"Array",elements:[{name:"signature",type:"object",raw:"{ text: string, icon?: string, className?: string }",signature:{properties:[{key:"text",value:{name:"string",required:!0}},{key:"icon",value:{name:"string",required:!1}},{key:"className",value:{name:"string",required:!1}}]}}],raw:"Array<{ text: string, icon?: string, className?: string }>"},description:"List of detail fields to be rendered above the blurb"},detailPageUrl:{required:!1,flowType:{name:"string"},description:"If a URL for a record detail page is provided, will render a button that links to it"},icon:{required:!1,flowType:{name:"string"},description:"The icon to display before the header."},onClose:{required:!1,flowType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:"A function called when the `close` icon is clicked"},onGoBack:{required:!1,flowType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:"A function called when the back arrow is clicked"},relations:{required:!1,flowType:{name:"Array",elements:[{name:"signature",type:"object",raw:`{
+  /**
+   * Icon to use in front of each list item. Defaults to none. Note this is overridden if a renderItem prop is provided
+   */
+  icon?: JSX.Element,
+
+  /**
+   * List of related items
+   */
+  items: Array<RelatedRecord>,
+
+  /**
+   * Optional render prop to render the title and count; defaults to \`\${title} (\${count})\`
+   */
+  renderTitle?: (title: String, count?: number | string) => JSX.Element,
+
+  /**
+   * Optional render prop to render each item in the list
+  */
+  renderItem?: (item: RelatedRecord) => JSX.Element,
+
+  /**
+   * The title of the related model
+   */
+  title: string,
+}`,signature:{properties:[{key:"icon",value:{name:"JSX.Element",required:!1},description:"Icon to use in front of each list item. Defaults to none. Note this is overridden if a renderItem prop is provided"},{key:"items",value:{name:"Array",elements:[{name:"signature",type:"object",raw:`{
+  /**
+   * Optional data prop to pass other fields, e.g. if needed for rendering
+  */
+  data?: any,
+
+  /**
+    * Optional event fired when item is clicked. Will be overridden if a renderItem prop is provided in the parent list
+  */
+  onClick?: () => void,
+
+  /**
+   * The primary name of the record (will display as text of the list item by default)
+   */
+  name: string,
+}`,signature:{properties:[{key:"data",value:{name:"any",required:!1},description:"Optional data prop to pass other fields, e.g. if needed for rendering"},{key:"onClick",value:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}},required:!1},description:"Optional event fired when item is clicked. Will be overridden if a renderItem prop is provided in the parent list"},{key:"name",value:{name:"string",required:!0},description:"The primary name of the record (will display as text of the list item by default)"}]}}],raw:"Array<RelatedRecord>",required:!0},description:"List of related items"},{key:"renderTitle",value:{name:"signature",type:"function",raw:"(title: String, count?: number | string) => JSX.Element",signature:{arguments:[{name:"title",type:{name:"String"}},{name:"count",type:{name:"union",raw:"number | string",elements:[{name:"number"},{name:"string"}]}}],return:{name:"JSX.Element"}},required:!1},description:"Optional render prop to render the title and count; defaults to `${title} (${count})`"},{key:"renderItem",value:{name:"signature",type:"function",raw:"(item: RelatedRecord) => JSX.Element",signature:{arguments:[{name:"item",type:{name:"signature",type:"object",raw:`{
+  /**
+   * Optional data prop to pass other fields, e.g. if needed for rendering
+  */
+  data?: any,
+
+  /**
+    * Optional event fired when item is clicked. Will be overridden if a renderItem prop is provided in the parent list
+  */
+  onClick?: () => void,
+
+  /**
+   * The primary name of the record (will display as text of the list item by default)
+   */
+  name: string,
+}`,signature:{properties:[{key:"data",value:{name:"any",required:!1},description:"Optional data prop to pass other fields, e.g. if needed for rendering"},{key:"onClick",value:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}},required:!1},description:"Optional event fired when item is clicked. Will be overridden if a renderItem prop is provided in the parent list"},{key:"name",value:{name:"string",required:!0},description:"The primary name of the record (will display as text of the list item by default)"}]}}}],return:{name:"JSX.Element"}},required:!1},description:"Optional render prop to render each item in the list"},{key:"title",value:{name:"string",required:!0},description:"The title of the related model"}]}}],raw:"Array<RelatedRecordsList>"},description:"An array of lists of related records for different model types"},title:{required:!0,flowType:{name:"string"},description:"The text of the header."}}};const he={title:"Components/Core Data/RecordDetailPanel",component:i},a=[{title:"Related People",items:[{name:"Kazuya Miyuki"},{name:"Eijun Sawamura"}],icon:"person",count:!0},{title:"Related Organizations",items:[{name:"Seido High School Baseball Club"},{name:"Yakushi High School Baseball Club"}],icon:"occupation",count:!0}],r=()=>e.jsx(i,{relations:a,title:"West Tokyo Qualifiers Quarterfinal",detailItems:[{text:"July 27",icon:"date"},{text:"Meiji Jinju Stadium",icon:"location"}],children:e.jsxs("p",{children:["Arcu imperdiet sit sit viverra id volutpat commodo."," ",e.jsx("span",{className:"font-bold",children:"Tempor sem malesuada porttitor congue."})," ","Nibh aenean vitae blandit vitae sapien ac varius mattis. Aliquam vitae purus arcu eros enim tempus parturient orci fames."]})}),n=()=>e.jsx(i,{relations:a,detailPageUrl:"#",title:"West Tokyo Qualifiers Quarterfinal",detailItems:[{text:"July 27",icon:"date"},{text:"Meiji Jinju Stadium",icon:"location"}],children:e.jsxs("p",{children:["Arcu imperdiet sit sit viverra id volutpat commodo."," ",e.jsx("span",{className:"font-bold",children:"Tempor sem malesuada porttitor congue."})," ","Nibh aenean vitae blandit vitae sapien ac varius mattis. Aliquam vitae purus arcu eros enim tempus parturient orci fames."]})}),o=()=>e.jsx(i,{relations:a,title:"West Tokyo Qualifiers Quarterfinal",detailItems:[{text:"July 27",icon:"date"},{text:"Meiji Jinju Stadium",icon:"location"}],icon:"participants",children:e.jsxs("p",{children:["Arcu imperdiet sit sit viverra id volutpat commodo."," ",e.jsx("span",{className:"font-bold",children:"Tempor sem malesuada porttitor congue."})," ","Nibh aenean vitae blandit vitae sapien ac varius mattis. Aliquam vitae purus arcu eros enim tempus parturient orci fames."]})}),s=()=>e.jsx(i,{relations:a,title:"West Tokyo Qualifiers Quarterfinal",detailItems:[{text:"July 27",icon:"date"},{text:"Meiji Jinju Stadium",icon:"location"}],onClose:()=>{alert("Closed!")},children:e.jsxs("p",{children:["Arcu imperdiet sit sit viverra id volutpat commodo."," ",e.jsx("span",{className:"font-bold",children:"Tempor sem malesuada porttitor congue."})," ","Nibh aenean vitae blandit vitae sapien ac varius mattis. Aliquam vitae purus arcu eros enim tempus parturient orci fames."]})}),l=()=>e.jsx(i,{relations:a,title:"West Tokyo Qualifiers Quarterfinal",detailItems:[{text:"July 27",icon:"date"},{text:"Meiji Jinju Stadium",icon:"location"}],breadcrumbs:["West Tokyo Qualifiers Semifinal","West Tokyo Qualifiers Quarterfinal"],onGoBack:()=>{alert("Go back!")},children:e.jsxs("p",{children:["Arcu imperdiet sit sit viverra id volutpat commodo."," ",e.jsx("span",{className:"font-bold",children:"Tempor sem malesuada porttitor congue."})," ","Nibh aenean vitae blandit vitae sapien ac varius mattis. Aliquam vitae purus arcu eros enim tempus parturient orci fames."]})}),d=()=>e.jsx(i,{relations:a,title:"West Tokyo Qualifiers Quarterfinal",detailItems:[{text:"July 27",icon:"date"},{text:"Meiji Jinju Stadium",icon:"location"}],breadcrumbs:["West Tokyo Qualifiers Semifinal","West Tokyo Qualifiers Quarterfinal"],onGoBack:()=>{alert("Go back!")},classNames:{root:"w-[380px] h-[500px]"},onClose:()=>{alert("Close!")},detailPageUrl:"#",children:e.jsxs("p",{children:["Arcu imperdiet sit sit viverra id volutpat commodo."," ",e.jsx("span",{className:"font-bold",children:"Tempor sem malesuada porttitor congue."})," ","Nibh aenean vitae blandit vitae sapien ac varius mattis. Aliquam vitae purus arcu eros enim tempus parturient orci fames."]})}),m=()=>e.jsx(i,{relations:a,title:"West Tokyo Qualifiers Quarterfinal",count:!0,detailItems:[{text:"July 27",icon:"date"},{text:"Meiji Jinju Stadium",icon:"location"}],children:e.jsxs("p",{children:["Arcu imperdiet sit sit viverra id volutpat commodo."," ",e.jsx("span",{className:"font-bold",children:"Tempor sem malesuada porttitor congue."})," ","Nibh aenean vitae blandit vitae sapien ac varius mattis. Aliquam vitae purus arcu eros enim tempus parturient orci fames."]})});r.__docgenInfo={description:"",methods:[],displayName:"Default"};n.__docgenInfo={description:"",methods:[],displayName:"WithViewDetail"};o.__docgenInfo={description:"",methods:[],displayName:"WithIcon"};s.__docgenInfo={description:"",methods:[],displayName:"WithClose"};l.__docgenInfo={description:"",methods:[],displayName:"WithBreadcrumbs"};d.__docgenInfo={description:"",methods:[],displayName:"FixedWidthAndHeight"};m.__docgenInfo={description:"",methods:[],displayName:"WithCounts"};var y,g,b;r.parameters={...r.parameters,docs:{...(y=r.parameters)==null?void 0:y.docs,source:{originalSource:`() => <RecordDetailPanel relations={sampleData} title='West Tokyo Qualifiers Quarterfinal' detailItems={[{
+  text: 'July 27',
+  icon: 'date'
+}, {
+  text: 'Meiji Jinju Stadium',
+  icon: 'location'
+}]}>
+    <p>
+      Arcu imperdiet sit sit viverra id volutpat commodo.
+      {' '}
+      <span className='font-bold'>Tempor sem malesuada porttitor congue.</span>
+      {' '}
+      Nibh aenean vitae blandit vitae sapien ac varius mattis.
+      Aliquam vitae purus arcu eros enim tempus parturient orci fames.
+    </p>
+  </RecordDetailPanel>`,...(b=(g=r.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};var x,k,j;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`() => <RecordDetailPanel relations={sampleData} detailPageUrl='#' title='West Tokyo Qualifiers Quarterfinal' detailItems={[{
+  text: 'July 27',
+  icon: 'date'
+}, {
+  text: 'Meiji Jinju Stadium',
+  icon: 'location'
+}]}>
+    <p>
+      Arcu imperdiet sit sit viverra id volutpat commodo.
+      {' '}
+      <span className='font-bold'>Tempor sem malesuada porttitor congue.</span>
+      {' '}
+      Nibh aenean vitae blandit vitae sapien ac varius mattis.
+      Aliquam vitae purus arcu eros enim tempus parturient orci fames.
+    </p>
+  </RecordDetailPanel>`,...(j=(k=n.parameters)==null?void 0:k.docs)==null?void 0:j.source}}};var T,N,w;o.parameters={...o.parameters,docs:{...(T=o.parameters)==null?void 0:T.docs,source:{originalSource:`() => <RecordDetailPanel relations={sampleData} title='West Tokyo Qualifiers Quarterfinal' detailItems={[{
+  text: 'July 27',
+  icon: 'date'
+}, {
+  text: 'Meiji Jinju Stadium',
+  icon: 'location'
+}]} icon='participants'>
+    <p>
+      Arcu imperdiet sit sit viverra id volutpat commodo.
+      {' '}
+      <span className='font-bold'>Tempor sem malesuada porttitor congue.</span>
+      {' '}
+      Nibh aenean vitae blandit vitae sapien ac varius mattis.
+      Aliquam vitae purus arcu eros enim tempus parturient orci fames.
+    </p>
+  </RecordDetailPanel>`,...(w=(N=o.parameters)==null?void 0:N.docs)==null?void 0:w.source}}};var A,q,I;s.parameters={...s.parameters,docs:{...(A=s.parameters)==null?void 0:A.docs,source:{originalSource:`() => <RecordDetailPanel relations={sampleData} title='West Tokyo Qualifiers Quarterfinal' detailItems={[{
+  text: 'July 27',
+  icon: 'date'
+}, {
+  text: 'Meiji Jinju Stadium',
+  icon: 'location'
+}]} onClose={() => {
+  alert('Closed!');
+}}>
+    <p>
+      Arcu imperdiet sit sit viverra id volutpat commodo.
+      {' '}
+      <span className='font-bold'>Tempor sem malesuada porttitor congue.</span>
+      {' '}
+      Nibh aenean vitae blandit vitae sapien ac varius mattis.
+      Aliquam vitae purus arcu eros enim tempus parturient orci fames.
+    </p>
+  </RecordDetailPanel>`,...(I=(q=s.parameters)==null?void 0:q.docs)==null?void 0:I.source}}};var W,Q,R;l.parameters={...l.parameters,docs:{...(W=l.parameters)==null?void 0:W.docs,source:{originalSource:`() => <RecordDetailPanel relations={sampleData} title='West Tokyo Qualifiers Quarterfinal' detailItems={[{
+  text: 'July 27',
+  icon: 'date'
+}, {
+  text: 'Meiji Jinju Stadium',
+  icon: 'location'
+}]} breadcrumbs={['West Tokyo Qualifiers Semifinal', 'West Tokyo Qualifiers Quarterfinal']} onGoBack={() => {
+  alert('Go back!');
+}}>
+    <p>
+      Arcu imperdiet sit sit viverra id volutpat commodo.
+      {' '}
+      <span className='font-bold'>Tempor sem malesuada porttitor congue.</span>
+      {' '}
+      Nibh aenean vitae blandit vitae sapien ac varius mattis.
+      Aliquam vitae purus arcu eros enim tempus parturient orci fames.
+    </p>
+  </RecordDetailPanel>`,...(R=(Q=l.parameters)==null?void 0:Q.docs)==null?void 0:R.source}}};var S,D,J;d.parameters={...d.parameters,docs:{...(S=d.parameters)==null?void 0:S.docs,source:{originalSource:`() => <RecordDetailPanel relations={sampleData} title='West Tokyo Qualifiers Quarterfinal' detailItems={[{
+  text: 'July 27',
+  icon: 'date'
+}, {
+  text: 'Meiji Jinju Stadium',
+  icon: 'location'
+}]} breadcrumbs={['West Tokyo Qualifiers Semifinal', 'West Tokyo Qualifiers Quarterfinal']} onGoBack={() => {
+  alert('Go back!');
+}} classNames={{
+  root: 'w-[380px] h-[500px]'
+}} onClose={() => {
+  alert('Close!');
+}} detailPageUrl='#'>
+    <p>
+      Arcu imperdiet sit sit viverra id volutpat commodo.
+      {' '}
+      <span className='font-bold'>Tempor sem malesuada porttitor congue.</span>
+      {' '}
+      Nibh aenean vitae blandit vitae sapien ac varius mattis.
+      Aliquam vitae purus arcu eros enim tempus parturient orci fames.
+    </p>
+  </RecordDetailPanel>`,...(J=(D=d.parameters)==null?void 0:D.docs)==null?void 0:J.source}}};var C,_,P;m.parameters={...m.parameters,docs:{...(C=m.parameters)==null?void 0:C.docs,source:{originalSource:`() => <RecordDetailPanel relations={sampleData} title='West Tokyo Qualifiers Quarterfinal' count detailItems={[{
+  text: 'July 27',
+  icon: 'date'
+}, {
+  text: 'Meiji Jinju Stadium',
+  icon: 'location'
+}]}>
+    <p>
+      Arcu imperdiet sit sit viverra id volutpat commodo.
+      {' '}
+      <span className='font-bold'>Tempor sem malesuada porttitor congue.</span>
+      {' '}
+      Nibh aenean vitae blandit vitae sapien ac varius mattis.
+      Aliquam vitae purus arcu eros enim tempus parturient orci fames.
+    </p>
+  </RecordDetailPanel>`,...(P=(_=m.parameters)==null?void 0:_.docs)==null?void 0:P.source}}};const ye=["Default","WithViewDetail","WithIcon","WithClose","WithBreadcrumbs","FixedWidthAndHeight","WithCounts"];export{r as Default,d as FixedWidthAndHeight,l as WithBreadcrumbs,s as WithClose,m as WithCounts,o as WithIcon,n as WithViewDetail,ye as __namedExportsOrder,he as default};
