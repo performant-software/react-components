21f0a5af61aff44bdefd4bca92f305d20b45e134
camdendotlol <c@camdenmecklem.com>
2025-02-17 17:31:52 +0000

    Deploying to gh-pages from @ performant-software/react-components@200a105bf5406b9495a6238b7519b1c60e45fe22 🚀

---
+++ docs/assets/RecordDetailHeader.stories-auZiURqy.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-D9TvR9R2.js";import"./index-C0dDX-lL.js";import{R as i}from"./RecordDetailHeader-BRRFRmhp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./index-default-C_maRo4o.js";import"./Button-DWHfM6Lf.js";import"./RecordDetailTitle-Bh36eBn7.js";import"./Icon-DNuTFG_h.js";import"./RecordDetailItem-DDmrWTl8.js";import"./i18n-D51Hdqse.js";import"./i18next-CwS9wMM1.js";import"./toPropertyKey-BcUJYDQU.js";import"./defineProperty-DcNpp85p.js";import"./inherits-CWPG5jll.js";import"./assertThisInitialized-B9jnkVVz.js";import"./setPrototypeOf-DgZC2w_0.js";import"./createClass-CV9nCQMI.js";import"./getPrototypeOf-BYVhAdwF.js";const E={title:"Components/Core Data/RecordDetailHeader",component:i},t=()=>e.jsx(i,{title:"A Very Serious and Important Event",detailItems:[{text:"March 11, 1986",icon:"date"},{text:"Princton, NJ",icon:"location"}],children:e.jsxs("p",{children:["Arcu imperdiet sit sit viverra id volutpat commodo."," ",e.jsx("span",{className:"font-bold",children:"Tempor sem malesuada porttitor congue."})," ","Nibh aenean vitae blandit vitae sapien ac varius mattis. Aliquam vitae purus arcu eros enim tempus parturient orci fames."]})}),a=()=>e.jsx(i,{title:"A Very Serious and Important Event",detailItems:[{text:"March 11, 1986",icon:"date"},{text:"Princton, NJ",icon:"location"}],detailPageUrl:"#",children:e.jsxs("p",{children:["Arcu imperdiet sit sit viverra id volutpat commodo."," ",e.jsx("span",{className:"font-bold",children:"Tempor sem malesuada porttitor congue."})," ","Nibh aenean vitae blandit vitae sapien ac varius mattis. Aliquam vitae purus arcu eros enim tempus parturient orci fames."]})});t.__docgenInfo={description:"",methods:[],displayName:"Default"};a.__docgenInfo={description:"",methods:[],displayName:"WithLink"};var r,o,n;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`() => <RecordDetailHeader title='A Very Serious and Important Event' detailItems={[{
+  text: 'March 11, 1986',
+  icon: 'date'
+}, {
+  text: 'Princton, NJ',
+  icon: 'location'
+}]}>
+    <p>
+      Arcu imperdiet sit sit viverra id volutpat commodo.
+      {' '}
+      <span className='font-bold'>Tempor sem malesuada porttitor congue.</span>
+      {' '}
+      Nibh aenean vitae blandit vitae sapien ac varius mattis.
+      Aliquam vitae purus arcu eros enim tempus parturient orci fames.
+    </p>
+  </RecordDetailHeader>`,...(n=(o=t.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var s,m,p;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`() => <RecordDetailHeader title='A Very Serious and Important Event' detailItems={[{
+  text: 'March 11, 1986',
+  icon: 'date'
+}, {
+  text: 'Princton, NJ',
+  icon: 'location'
+}]} detailPageUrl='#'>
+    <p>
+      Arcu imperdiet sit sit viverra id volutpat commodo.
+      {' '}
+      <span className='font-bold'>Tempor sem malesuada porttitor congue.</span>
+      {' '}
+      Nibh aenean vitae blandit vitae sapien ac varius mattis.
+      Aliquam vitae purus arcu eros enim tempus parturient orci fames.
+    </p>
+  </RecordDetailHeader>`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const P=["Default","WithLink"];export{t as Default,a as WithLink,P as __namedExportsOrder,E as default};
