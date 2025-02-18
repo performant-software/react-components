d238451a9f14a05a611742f30753e7cb0a9a959a
dleadbetter <20641961+dleadbetter@users.noreply.github.com>
2025-02-18 16:26:20 +0000

    Deploying to gh-pages from @ performant-software/react-components@7018ace29dad522ca0ca535e78f07459e3e6fd2d 🚀

---
+++ docs/assets/Event-Bij_IVWI.js
@@ -0,0 +1,1 @@
+import{G as e}from"./index.es33-D7u3vlC1.js";import"./index-C0dDX-lL.js";import{_ as o}from"./index-default-C_maRo4o.js";const a=t=>o.compact([e.getDateView(t.start_date),e.getDateView(t.end_date)]).join(" - "),p={getDateView:a};export{p as E};
