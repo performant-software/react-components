f6831612ebc51e901dfd90e2c1e9f87abffb9dbb
Camden Mecklem <c@camdenmecklem.com>
2025-03-18 14:29:02 -0400

    import ssr.js in index.js

--- packages/core-data/src/index.js
+++ packages/core-data/src/index.js
@@ -3,74 +3,36 @@
 // CSS
 import './index.css';
 
+export * from './ssr';
+
 // Components
-export { default as AccordionItemsList } from './components/AccordionItemsList';
-export { default as Button } from './components/Button';
-export { default as ButtonGroup } from './components/ButtonGroup';
-export { default as Checkbox } from './components/Checkbox';
-export { default as Combobox } from './components/Combobox';
-export { default as CoreDataContextProvider } from './components/CoreDataContextProvider';
-export { default as EventDetails } from './components/EventDetails';
-export { default as EventsList } from './components/EventsList';
 export { default as FacetList } from './components/FacetList';
 export { default as FacetLists } from './components/FacetLists';
 export { default as FacetListsGrouped } from './components/FacetListsGrouped';
 export { default as FacetSlider } from './components/FacetSlider';
 export { default as FacetStateContextProvider } from './components/FacetStateContextProvider';
 export { default as FacetTimeline } from './components/FacetTimeline';
-export { default as HeaderImage } from './components/HeaderImage';
 export { default as HitsPerPage } from './components/HitsPerPage';
-export { default as Icon } from './components/Icon';
-export { default as Input } from './components/Input';
-export { default as KeyValueList } from './components/KeyValueList';
 export { default as LayerMenu } from './components/LayerMenu';
-export { default as LoadAnimation } from './components/LoadAnimation';
 export { default as MediaGallery } from './components/MediaGallery';
-export { default as Modal } from './components/Modal';
 export { default as OverlayLayers } from './components/OverlayLayers';
-export { default as Pagination } from './components/Pagination';
 export { default as PersistentSearchStateContextProvider } from './components/PersistentSearchStateContextProvider';
-export { default as Pill } from './components/Pill';
-export { default as PlaceDetails } from './components/PlaceDetails';
 export { default as PlaceLayersSelector } from './components/PlaceLayersSelector';
 export { default as PlaceMarkers } from './components/PlaceMarkers';
 export { default as SearchResultsList } from './components/SearchResultsList';
 export { default as RecordDetailPanel } from './components/RecordDetailPanel';
 export { default as RefinementListProxy } from './components/RefinementListProxy';
-export { default as RelatedEvents } from './components/RelatedEvents';
-export { default as RelatedItem } from './components/RelatedItem';
-export { default as RelatedItems } from './components/RelatedItems';
-export { default as RelatedItemsList } from './components/RelatedItemsList';
-export { default as RelatedList } from './components/RelatedList';
 export { default as RelatedMedia } from './components/RelatedMedia';
-export { default as RelatedOrganizations } from './components/RelatedOrganizations';
-export { default as RelatedPeople } from './components/RelatedPeople';
-export { default as RelatedPlaces } from './components/RelatedPlaces';
 export { default as RelatedPlacesLayer } from './components/RelatedPlacesLayer';
-export { default as RelatedSources } from './components/RelatedSources';
-export { default as RelatedTaxonomies } from './components/RelatedTaxonomies';
 export { default as SearchList } from './components/SearchList';
 export { default as SearchResultsLayer } from './components/SearchResultsLayer';
-export { default as SearchResultsTable } from './components/SearchResultsTable';
-export { default as SelectRecordPanel } from './components/SelectRecordPanel';
 export { default as Slider } from './components/Slider';
 
 // Contexts
-export { default as CoreDataContext } from './context/CoreData';
 export { default as FacetStateContext } from './context/FacetStateContext';
 export { default as PersistentSearchStateContext } from './context/PersistentSearchState';
 
 // Hooks
-export {
-  useEventsService,
-  useInstancesService,
-  useItemsService,
-  useLoader,
-  useOrganizationsService,
-  usePeopleService,
-  usePlacesService,
-  useWorksService
-} from './hooks/CoreData';
 export { default as useProgressiveSearch } from './hooks/ProgressiveSearch';
 export {
   useCachedHits,
@@ -81,18 +43,6 @@ export {
   useSearching
 } from './hooks/Typesense';
 
-// Services
-export { default as EventsService } from './services/Events';
-export { default as InstancesService } from './services/Instances';
-export { default as ItemsService } from './services/Items';
-export { default as OrganizationsService } from './services/Organizations';
-export { default as PeopleService } from './services/People';
-export { default as PlacesService } from './services/Places';
-export { default as WorksService } from './services/Works';
-
 // Utilities
-export { default as Api } from './utils/Api';
-export { default as CoreData } from './utils/CoreData';
-export { default as Event } from './utils/Event';
 export { default as Peripleo } from './utils/Peripleo';
 export { default as Typesense } from './utils/Typesense';
