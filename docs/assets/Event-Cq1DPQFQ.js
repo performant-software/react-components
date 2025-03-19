c42d28d619abca6d477222cb1c515b548ef2ce35
dleadbetter <20641961+dleadbetter@users.noreply.github.com>
2025-03-19 14:26:36 +0000

    Deploying to gh-pages from @ performant-software/react-components@7088ee7164c3fb180541d6fb185a4249439836eb 🚀

--- docs/assets/Event-Cq1DPQFQ.js
+++
@@ -1,1 +0,0 @@
-import{G as e}from"./index.es33-DlV9y7LU.js";import"./index-C0dDX-lL.js";import{_ as o}from"./index-default-C_maRo4o.js";const a=t=>o.compact([e.getDateView(t.start_date),e.getDateView(t.end_date)]).join(" - "),p={getDateView:a};export{p as E};
