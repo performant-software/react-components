9af9ae11de6e84f76ddbf3ea07e6175b5f1a3473
blms <ben.lm.silverman@gmail.com>
2025-03-25 13:26:53 +0000

    Deploying to gh-pages from @ performant-software/react-components@92729bf264fd1f6c2739d029f86282b0fb5432d6 🚀

---
+++ docs/assets/Facet.stories-DCKqqXGn.js
@@ -0,0 +1,120 @@
+import{j as e}from"./jsx-runtime-D9TvR9R2.js";import{d as m}from"./index-Dtq_bT1O.js";import{r as c}from"./index-C0dDX-lL.js";import{F as n}from"./Form-WfNl3YLh.js";import{B as F}from"./Button-CJhT71wt.js";import{C as l}from"./Checkbox-B3tsCUtI.js";import{F as i}from"./Facet-DQxf_TaR.js";import"./mapValues-BntjSFt6.js";import"./_commonjsHelpers-BosuxZz1.js";import"./pickBy-CC7CsC_n.js";import"./_commonjs-dynamic-modules-TDtrdbi3.js";import"./isPlainObject-D9-U-Vjl.js";import"./index-Cw1G72V2.js";import"./index-CKJfshIT.js";import"./tiny-invariant.cjs-DxxScMW_.js";import"./___vite-browser-external_commonjs-proxy-DRaEfepn.js";import"./preview-errors-CzpZmerH.js";import"./debounce-BANtZgGn.js";import"./extends-CCbyfPlC.js";import"./inheritsLoose-1HkgTRWY.js";import"./setPrototypeOf-DgZC2w_0.js";import"./getElementType-BSRayMAs.js";import"./Label-aEFd48OX.js";import"./ModernAutoControlledComponent-C211dBIC.js";import"./assertThisInitialized-B9jnkVVz.js";import"./Icon-C_3d7-zE.js";import"./includes-Cc_HXDPA.js";import"./toFinite-C3oJXAtX.js";import"./forEach-CLeEMxs1.js";import"./Dimmer-DP3fvqlo.js";import"./Portal-B8DXQ6EK.js";import"./keyboardKey-OMDgGVm3.js";import"./index-DOYXiIEK.js";import"./objectWithoutPropertiesLoose-1QL-frMM.js";import"./Dropdown-DwNow7wJ.js";import"./map-DWKMCCtW.js";import"./without-Duh6Zu7H.js";import"./isArrayLikeObject-Bl_bAmop.js";import"./isEmpty-CY-Bv0TY.js";import"./deburr-CNLr_JRW.js";import"./index-mbjjMjCk.js";import"./_baseSet-BllfnHSO.js";import"./_baseAssignValue-UQlBDUKN.js";import"./_arrayReduce-CrxnWFSq.js";import"./filter-B6f9p5im.js";import"./Input-Bwh_HEb3.js";import"./Divider-CUCHYA5N.js";import"./Header-BHdMo175.js";import"./Accordion-61xP-IaT.js";const De={title:"Components/Semantic UI/Facet",component:i},t=()=>e.jsx(i,{title:"Department",children:e.jsxs(n,{children:[e.jsx(n.Field,{children:e.jsx(l,{label:"Appliances"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Bath & Faucets"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Building Materials"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Doors & Windows"})})]})}),a=()=>e.jsx(i,{defaultActive:!1,title:"Department",children:e.jsxs(n,{children:[e.jsx(n.Field,{children:e.jsx(l,{label:"Appliances"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Bath & Faucets"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Building Materials"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Doors & Windows"})})]})}),s=()=>e.jsxs(e.Fragment,{children:[e.jsx(i,{divided:m.boolean("Divided",!0),title:"Department",children:e.jsxs(n,{children:[e.jsx(n.Field,{children:e.jsx(l,{label:"Appliances"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Bath & Faucets"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Building Materials"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Doors & Windows"})})]})}),e.jsx(i,{divided:m.boolean("Divided",!0),title:"Brand",children:e.jsxs(n,{children:[e.jsx(n.Field,{children:e.jsx(l,{label:"American Standard"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Andersen"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Delta"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Kohler"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"LG"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Samsung"})})]})})]}),d=()=>{const r=c.useRef(),v=c.useCallback(()=>{const{current:o}=r;o&&o.collapse()},[r.current]),A=c.useCallback(()=>{const{current:o}=r;o&&o.expand()},[r.current]);return e.jsxs(e.Fragment,{children:[e.jsx(i,{innerRef:r,title:"Department",children:e.jsxs(n,{children:[e.jsx(n.Field,{children:e.jsx(l,{label:"Appliances"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Bath & Faucets"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Building Materials"})}),e.jsx(n.Field,{children:e.jsx(l,{label:"Doors & Windows"})})]})}),e.jsx(F,{content:"Expand",onClick:A}),e.jsx(F,{content:"Collapse",onClick:v})]})};t.__docgenInfo={description:"",methods:[],displayName:"Default"};a.__docgenInfo={description:"",methods:[],displayName:"Collapsed"};s.__docgenInfo={description:"",methods:[],displayName:"Divided"};d.__docgenInfo={description:"",methods:[],displayName:"ExpandCollapse"};var p,x,b;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`() => <Facet title='Department'>
+    <Form>
+      <Form.Field>
+        <Checkbox label='Appliances' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Bath & Faucets' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Building Materials' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Doors & Windows' />
+      </Form.Field>
+    </Form>
+  </Facet>`,...(b=(x=t.parameters)==null?void 0:x.docs)==null?void 0:b.source}}};var h,u,j;a.parameters={...a.parameters,docs:{...(h=a.parameters)==null?void 0:h.docs,source:{originalSource:`() => <Facet defaultActive={false} title='Department'>
+    <Form>
+      <Form.Field>
+        <Checkbox label='Appliances' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Bath & Faucets' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Building Materials' />
+      </Form.Field>
+      <Form.Field>
+        <Checkbox label='Doors & Windows' />
+      </Form.Field>
+    </Form>
+  </Facet>`,...(j=(u=a.parameters)==null?void 0:u.docs)==null?void 0:j.source}}};var C,f,k;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`() => <>
+    <Facet divided={boolean('Divided', true)} title='Department'>
+      <Form>
+        <Form.Field>
+          <Checkbox label='Appliances' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Bath & Faucets' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Building Materials' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Doors & Windows' />
+        </Form.Field>
+      </Form>
+    </Facet>
+    <Facet divided={boolean('Divided', true)} title='Brand'>
+      <Form>
+        <Form.Field>
+          <Checkbox label='American Standard' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Andersen' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Delta' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Kohler' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='LG' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='Samsung' />
+        </Form.Field>
+      </Form>
+    </Facet>
+  </>`,...(k=(f=s.parameters)==null?void 0:f.docs)==null?void 0:k.source}}};var D,B,g;d.parameters={...d.parameters,docs:{...(D=d.parameters)==null?void 0:D.docs,source:{originalSource:`() => {
+  const ref = useRef();
+
+  /**
+   * Collapses the facet via the imperative function.
+   *
+   * @type {(function(): void)|*}
+   */
+  const onCollapse = useCallback(() => {
+    const {
+      current: instance
+    } = ref;
+    if (instance) {
+      instance.collapse();
+    }
+  }, [ref.current]);
+
+  /**
+   * Expands the facet via the imperative function.
+   *
+   * @type {(function(): void)|*}
+   */
+  const onExpand = useCallback(() => {
+    const {
+      current: instance
+    } = ref;
+    if (instance) {
+      instance.expand();
+    }
+  }, [ref.current]);
+  return <>
+      <Facet innerRef={ref} title='Department'>
+        <Form>
+          <Form.Field>
+            <Checkbox label='Appliances' />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label='Bath & Faucets' />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label='Building Materials' />
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label='Doors & Windows' />
+          </Form.Field>
+        </Form>
+      </Facet>
+      <Button content='Expand' onClick={onExpand} />
+      <Button content='Collapse' onClick={onCollapse} />
+    </>;
+}`,...(g=(B=d.parameters)==null?void 0:B.docs)==null?void 0:g.source}}};const Be=["Default","Collapsed","Divided","ExpandCollapse"];export{a as Collapsed,t as Default,s as Divided,d as ExpandCollapse,Be as __namedExportsOrder,De as default};
---
+++ docs/assets/MenuSidebar.stories-V5H9S3Gp.js
@@ -0,0 +1,54 @@
+import{j as i}from"./jsx-runtime-D9TvR9R2.js";import"./index-C0dDX-lL.js";import{d as r}from"./index-Dtq_bT1O.js";import{M as c}from"./MenuSidebar-D9z-_GwB.js";import"./_commonjsHelpers-BosuxZz1.js";import"./mapValues-BntjSFt6.js";import"./pickBy-CC7CsC_n.js";import"./_commonjs-dynamic-modules-TDtrdbi3.js";import"./isPlainObject-D9-U-Vjl.js";import"./index-Cw1G72V2.js";import"./index-CKJfshIT.js";import"./tiny-invariant.cjs-DxxScMW_.js";import"./___vite-browser-external_commonjs-proxy-DRaEfepn.js";import"./preview-errors-CzpZmerH.js";import"./debounce-BANtZgGn.js";import"./Menu-C3RUPz9h.js";import"./extends-CCbyfPlC.js";import"./inheritsLoose-1HkgTRWY.js";import"./setPrototypeOf-DgZC2w_0.js";import"./map-DWKMCCtW.js";import"./getElementType-BSRayMAs.js";import"./ModernAutoControlledComponent-C211dBIC.js";import"./assertThisInitialized-B9jnkVVz.js";import"./_arrayReduce-CrxnWFSq.js";import"./deburr-CNLr_JRW.js";import"./Icon-C_3d7-zE.js";import"./Header-BHdMo175.js";import"./Label-aEFd48OX.js";import"./includes-Cc_HXDPA.js";import"./toFinite-C3oJXAtX.js";import"./forEach-CLeEMxs1.js";import"./Dimmer-DP3fvqlo.js";import"./Portal-B8DXQ6EK.js";import"./keyboardKey-OMDgGVm3.js";import"./index-DOYXiIEK.js";import"./objectWithoutPropertiesLoose-1QL-frMM.js";import"./index-default-C_maRo4o.js";const F={title:"Components/Semantic UI/MenuSidebar",decorators:[r.withKnobs]},n=()=>i.jsx(c,{header:{content:"Test Application"},items:[{content:"Products",items:[{content:"Enterprise",onClick:()=>{}},{content:"Consumer",onClick:()=>{}}]},{content:"CMS Solution",items:[{content:"Rails",onClick:()=>{}},{content:"Python",onClick:()=>{}},{content:"PHP",onClick:()=>{}}]},{content:"Hosting",items:[{content:"Shared",onClick:()=>{}},{content:"Dedicated",onClick:()=>{}}]},{content:"Clothing",items:[{content:"Mens",items:[{content:"T-Shirts",onClick:()=>{}},{content:"Pants",onClick:()=>{}}]},{content:"Womens",items:[{content:"T-Shirts",onClick:()=>{}},{content:"Dresses",onClick:()=>{}}]}]}]});n.__docgenInfo={description:"",methods:[],displayName:"Default"};var t,o,e;n.parameters={...n.parameters,docs:{...(t=n.parameters)==null?void 0:t.docs,source:{originalSource:`() => <MenuSidebar header={{
+  content: 'Test Application'
+}} items={[{
+  content: 'Products',
+  items: [{
+    content: 'Enterprise',
+    onClick: () => {}
+  }, {
+    content: 'Consumer',
+    onClick: () => {}
+  }]
+}, {
+  content: 'CMS Solution',
+  items: [{
+    content: 'Rails',
+    onClick: () => {}
+  }, {
+    content: 'Python',
+    onClick: () => {}
+  }, {
+    content: 'PHP',
+    onClick: () => {}
+  }]
+}, {
+  content: 'Hosting',
+  items: [{
+    content: 'Shared',
+    onClick: () => {}
+  }, {
+    content: 'Dedicated',
+    onClick: () => {}
+  }]
+}, {
+  content: 'Clothing',
+  items: [{
+    content: 'Mens',
+    items: [{
+      content: 'T-Shirts',
+      onClick: () => {}
+    }, {
+      content: 'Pants',
+      onClick: () => {}
+    }]
+  }, {
+    content: 'Womens',
+    items: [{
+      content: 'T-Shirts',
+      onClick: () => {}
+    }, {
+      content: 'Dresses',
+      onClick: () => {}
+    }]
+  }]
+}]} />`,...(e=(o=n.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const G=["Default"];export{n as Default,G as __namedExportsOrder,F as default};
