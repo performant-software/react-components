b64f8973a0bc3847f77c9a0dc3308f7570de8ac1
blms <ben.lm.silverman@gmail.com>
2025-04-01 13:59:57 +0000

    Deploying to gh-pages from @ performant-software/react-components@824c1728ebd561ec8dce7d2640d004db63d3ce6c 🚀

---
+++ docs/assets/ButtonGroup.stories-CnP3oR2f.js
@@ -0,0 +1,27 @@
+import{j as t}from"./jsx-runtime-D9TvR9R2.js";import{r as d}from"./index-C0dDX-lL.js";import{B as n}from"./Button-DHdN4eAR.js";import{B as s}from"./ButtonGroup-1FXbxVEl.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";const k={title:"Components/Core Data/ButtonGroup",component:s},r=()=>{const[e,i]=d.useState("list");return t.jsxs(s,{children:[t.jsx(n,{onClick:()=>i("list"),primary:e==="list",children:"List"}),t.jsx(n,{onClick:()=>i("table"),primary:e==="table",children:"Table"}),t.jsx(n,{onClick:()=>i("timeline"),primary:e==="timeline",children:"Timeline"})]})},o=()=>{const[e,i]=d.useState("list");return t.jsxs(s,{rounded:!0,children:[t.jsx(n,{onClick:()=>i("list"),primary:e==="list",children:"List"}),t.jsx(n,{onClick:()=>i("table"),primary:e==="table",children:"Table"}),t.jsx(n,{onClick:()=>i("timeline"),primary:e==="timeline",children:"Timeline"})]})};r.__docgenInfo={description:"",methods:[],displayName:"Default"};o.__docgenInfo={description:"",methods:[],displayName:"Rounded"};var a,c,l;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`() => {
+  const [active, setActive] = useState('list');
+  return <ButtonGroup>
+      <Button onClick={() => setActive('list')} primary={active === 'list'}>
+        List
+      </Button>
+      <Button onClick={() => setActive('table')} primary={active === 'table'}>
+        Table
+      </Button>
+      <Button onClick={() => setActive('timeline')} primary={active === 'timeline'}>
+        Timeline
+      </Button>
+    </ButtonGroup>;
+}`,...(l=(c=r.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var u,m,p;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`() => {
+  const [active, setActive] = useState('list');
+  return <ButtonGroup rounded>
+      <Button onClick={() => setActive('list')} primary={active === 'list'}>
+        List
+      </Button>
+      <Button onClick={() => setActive('table')} primary={active === 'table'}>
+        Table
+      </Button>
+      <Button onClick={() => setActive('timeline')} primary={active === 'timeline'}>
+        Timeline
+      </Button>
+    </ButtonGroup>;
+}`,...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const f=["Default","Rounded"];export{r as Default,o as Rounded,f as __namedExportsOrder,k as default};
---
+++ docs/assets/EventsList-DplLP1QB.js
@@ -0,0 +1,55 @@
+import{j as e}from"./jsx-runtime-D9TvR9R2.js";import{c as a}from"./clsx-B-dksMZM.js";import"./index-C0dDX-lL.js";import{_ as u}from"./index-default-C_maRo4o.js";import{E as i}from"./Event-DT2DaYEO.js";const n=r=>e.jsx("ul",{className:r.className,children:u.map(r.events,t=>e.jsx("li",{children:e.jsx("div",{className:"min-h-[5.5em] border-b flex flex-col justify-start",children:e.jsxs("button",{className:a("py-3","px-4","flex-grow","text-left","inline-flex","flex-col","rounded-none",{"hover:bg-event-selected":r.isSelected(t)},{"text-white":r.isSelected(t)},{"bg-event-selected":r.isSelected(t)}),onClick:()=>r.onClick(t),type:"button",children:[e.jsx("div",{className:"flex justify-between w-full items-center",children:e.jsxs("div",{className:"flex-grow",children:[e.jsx("div",{children:i.getDateView(t)}),e.jsx("h2",{className:"text-xl font-bold",children:t.name})]})}),r.description&&e.jsx("p",{className:a("py-2",{"text-muted":!r.isSelected(t)}),children:t.description})]})})}))});n.defaultProps={isSelected:()=>!1,onClick:()=>{}};n.__docgenInfo={description:"",methods:[],displayName:"EventsList",props:{className:{required:!1,flowType:{name:"string"},description:"(Optional) class name to apply to the root element."},description:{required:!1,flowType:{name:"boolean"},description:"If `true`, the event description will be displayed on the card."},events:{required:!0,flowType:{name:"Array",elements:[{name:"signature",type:"object",raw:`{
+  uuid: string,
+  name: string,
+  description: string,
+  start_date: FuzzyDate,
+  end_date: FuzzyDate
+}`,signature:{properties:[{key:"uuid",value:{name:"string",required:!0}},{key:"name",value:{name:"string",required:!0}},{key:"description",value:{name:"string",required:!0}},{key:"start_date",value:{name:"signature",type:"object",raw:`{
+  accuracy: number,
+  range: boolean,
+  description: string,
+  start_date: Date,
+  end_date: Date
+}`,signature:{properties:[{key:"accuracy",value:{name:"number",required:!0}},{key:"range",value:{name:"boolean",required:!0}},{key:"description",value:{name:"string",required:!0}},{key:"start_date",value:{name:"Date",required:!0}},{key:"end_date",value:{name:"Date",required:!0}}]},required:!0}},{key:"end_date",value:{name:"signature",type:"object",raw:`{
+  accuracy: number,
+  range: boolean,
+  description: string,
+  start_date: Date,
+  end_date: Date
+}`,signature:{properties:[{key:"accuracy",value:{name:"number",required:!0}},{key:"range",value:{name:"boolean",required:!0}},{key:"description",value:{name:"string",required:!0}},{key:"start_date",value:{name:"Date",required:!0}},{key:"end_date",value:{name:"Date",required:!0}}]},required:!0}}]}}],raw:"Array<EventType>"},description:"The list of events records to display."},isSelected:{required:!1,flowType:{name:"signature",type:"function",raw:"(event: EventType) => boolean",signature:{arguments:[{name:"event",type:{name:"signature",type:"object",raw:`{
+  uuid: string,
+  name: string,
+  description: string,
+  start_date: FuzzyDate,
+  end_date: FuzzyDate
+}`,signature:{properties:[{key:"uuid",value:{name:"string",required:!0}},{key:"name",value:{name:"string",required:!0}},{key:"description",value:{name:"string",required:!0}},{key:"start_date",value:{name:"signature",type:"object",raw:`{
+  accuracy: number,
+  range: boolean,
+  description: string,
+  start_date: Date,
+  end_date: Date
+}`,signature:{properties:[{key:"accuracy",value:{name:"number",required:!0}},{key:"range",value:{name:"boolean",required:!0}},{key:"description",value:{name:"string",required:!0}},{key:"start_date",value:{name:"Date",required:!0}},{key:"end_date",value:{name:"Date",required:!0}}]},required:!0}},{key:"end_date",value:{name:"signature",type:"object",raw:`{
+  accuracy: number,
+  range: boolean,
+  description: string,
+  start_date: Date,
+  end_date: Date
+}`,signature:{properties:[{key:"accuracy",value:{name:"number",required:!0}},{key:"range",value:{name:"boolean",required:!0}},{key:"description",value:{name:"string",required:!0}},{key:"start_date",value:{name:"Date",required:!0}},{key:"end_date",value:{name:"Date",required:!0}}]},required:!0}}]}}}],return:{name:"boolean"}}},description:"Callback that returns `true` if the passed event is selected.",defaultValue:{value:"() => false",computed:!1}},onClick:{required:!1,flowType:{name:"signature",type:"function",raw:"(event: EventType) => void",signature:{arguments:[{name:"event",type:{name:"signature",type:"object",raw:`{
+  uuid: string,
+  name: string,
+  description: string,
+  start_date: FuzzyDate,
+  end_date: FuzzyDate
+}`,signature:{properties:[{key:"uuid",value:{name:"string",required:!0}},{key:"name",value:{name:"string",required:!0}},{key:"description",value:{name:"string",required:!0}},{key:"start_date",value:{name:"signature",type:"object",raw:`{
+  accuracy: number,
+  range: boolean,
+  description: string,
+  start_date: Date,
+  end_date: Date
+}`,signature:{properties:[{key:"accuracy",value:{name:"number",required:!0}},{key:"range",value:{name:"boolean",required:!0}},{key:"description",value:{name:"string",required:!0}},{key:"start_date",value:{name:"Date",required:!0}},{key:"end_date",value:{name:"Date",required:!0}}]},required:!0}},{key:"end_date",value:{name:"signature",type:"object",raw:`{
+  accuracy: number,
+  range: boolean,
+  description: string,
+  start_date: Date,
+  end_date: Date
+}`,signature:{properties:[{key:"accuracy",value:{name:"number",required:!0}},{key:"range",value:{name:"boolean",required:!0}},{key:"description",value:{name:"string",required:!0}},{key:"start_date",value:{name:"Date",required:!0}},{key:"end_date",value:{name:"Date",required:!0}}]},required:!0}}]}}}],return:{name:"void"}}},description:"Callback fired when the event row is clicked.",defaultValue:{value:"() => {}",computed:!1}}}};export{n as E};
