59814cb8153b28aae761a77ebb080362c433b2a6
dleadbetter <20641961+dleadbetter@users.noreply.github.com>
2025-04-08 14:43:03 +0000

    Deploying to gh-pages from @ performant-software/react-components@45251c2b8941c1751c40074aa5480f6998cf7d6f 🚀

---
+++ docs/assets/LazyImage.stories-Dp5lDC3c.js
@@ -0,0 +1,63 @@
+import{j as e}from"./jsx-runtime-D9TvR9R2.js";import"./index-C0dDX-lL.js";import{a as h}from"./chunk-WFFRPTHA-B2vgThZf.js";import{B as u}from"./Button-CJhT71wt.js";import{C as o}from"./Card-ClzD5NPk.js";import{I as a}from"./Item-DsGqipCH.js";import{i as t}from"./test-image-BGrKnxUR.js";import{L as r}from"./LazyImage-CzTHm740.js";import{p as M}from"./portrait-test-image-CFX7vfGN.js";import"./_commonjsHelpers-BosuxZz1.js";import"./preview-errors-CzpZmerH.js";import"./index-CKJfshIT.js";import"./v4-D8aEg3BZ.js";import"./extends-CCbyfPlC.js";import"./inheritsLoose-1HkgTRWY.js";import"./setPrototypeOf-DgZC2w_0.js";import"./getElementType-BSRayMAs.js";import"./Icon-C_3d7-zE.js";import"./Label-aEFd48OX.js";import"./ModernAutoControlledComponent-C211dBIC.js";import"./assertThisInitialized-B9jnkVVz.js";import"./includes-Cc_HXDPA.js";import"./toFinite-C3oJXAtX.js";import"./forEach-CLeEMxs1.js";import"./Dimmer-DP3fvqlo.js";import"./Portal-B8DXQ6EK.js";import"./keyboardKey-OMDgGVm3.js";import"./index-DOYXiIEK.js";import"./objectWithoutPropertiesLoose-1QL-frMM.js";import"./map-DWKMCCtW.js";import"./LazyLoader-DWAGao17.js";import"./without-Duh6Zu7H.js";import"./isArrayLikeObject-Bl_bAmop.js";import"./Loader-lqR_Ikjg.js";import"./Segment-BqjJDZFO.js";import"./Transition-HA1Bivu3.js";import"./_baseAssignValue-UQlBDUKN.js";import"./filter-B6f9p5im.js";import"./i18n-C9xJ7SCg.js";import"./i18next-CwS9wMM1.js";import"./toPropertyKey-BcUJYDQU.js";import"./defineProperty-DcNpp85p.js";import"./inherits-CWPG5jll.js";import"./createClass-CV9nCQMI.js";import"./getPrototypeOf-BYVhAdwF.js";import"./DownloadButton-f81vwX5G.js";import"./uuid-BH6G0oTz.js";import"./PhotoViewer-C-XbURE_.js";import"./index.es33-ByTDBih8.js";import"./iframe-3OkfUO15.js";import"../sb-preview/runtime.js";import"./Message-smmiVK7n.js";import"./Modal-BWgkEMr1.js";import"./index-mbjjMjCk.js";import"./_baseSet-BllfnHSO.js";import"./_arrayReduce-CrxnWFSq.js";import"./isEmpty-CY-Bv0TY.js";const Ye={title:"Components/Semantic UI/LazyImage"},m=()=>e.jsx(r,{image:{alt:"Image"},src:t}),s=()=>e.jsx(r,{}),i=()=>e.jsxs(r,{image:{alt:"Test Image"},src:t,children:[e.jsx(u,{color:"green",content:"Edit photo",icon:"edit",onClick:h("edit")}),e.jsx(u,{color:"orange",content:"Change photo",icon:"move",onClick:h("change")}),e.jsx(u,{color:"red",content:"Delete photo",icon:"trash",onClick:h("delete")})]}),n=()=>e.jsxs(o.Group,{children:[e.jsxs(o,{children:[e.jsx(r,{image:{alt:"Test Image"},src:t}),e.jsx(o.Content,{header:"Test 1"})]}),e.jsxs(o,{children:[e.jsx(r,{image:{alt:"Portrait Image"},src:M}),e.jsx(o.Content,{header:"Test 2"})]}),e.jsxs(o,{children:[e.jsx(r,{image:{alt:"Last Image"}}),e.jsx(o.Content,{header:"Test 3"})]})]}),c=()=>e.jsxs(a.Group,{children:[e.jsxs(a,{children:[e.jsx(a.Image,{children:e.jsx(r,{image:{alt:"First Image"},src:t})}),e.jsx(a.Content,{header:"Test 1"})]}),e.jsxs(a,{children:[e.jsx(a.Image,{children:e.jsx(r,{image:{alt:"Portrait Image"},src:M})}),e.jsx(a.Content,{image:{alt:"Last Image"},header:"Test 2"})]}),e.jsxs(a,{children:[e.jsx(a.Image,{children:e.jsx(r,{})}),e.jsx(a.Content,{header:"Test 3"})]})]}),p=()=>e.jsx(r,{image:{alt:"Image"},src:"path/to/image.jpg"}),g=()=>e.jsx(r,{image:{alt:"Image"},preview:t,src:"path/to/image.jpg"}),d=()=>e.jsx(r,{image:{alt:"Image"},preview:"path/to/image.jpg",src:t}),l=()=>e.jsx(r,{image:{alt:"Image"},preview:"path/to/image.jpg",src:"path/to/image.jpg"}),I=()=>e.jsx(r,{downloadUrl:t,name:"Image.jpg",image:{alt:"Image"},src:t});m.__docgenInfo={description:"",methods:[],displayName:"Default"};s.__docgenInfo={description:"",methods:[],displayName:"Placeholder"};i.__docgenInfo={description:"",methods:[],displayName:"ExtraButtons"};n.__docgenInfo={description:"",methods:[],displayName:"CardContent"};c.__docgenInfo={description:"",methods:[],displayName:"ListContent"};p.__docgenInfo={description:"",methods:[],displayName:"ErrorSource"};g.__docgenInfo={description:"",methods:[],displayName:"ErrorSourceWithPreview"};d.__docgenInfo={description:"",methods:[],displayName:"ErrorPreview"};l.__docgenInfo={description:"",methods:[],displayName:"ErrorPreviewAndSource"};I.__docgenInfo={description:"",methods:[],displayName:"Downloadable"};var j,C,x;m.parameters={...m.parameters,docs:{...(j=m.parameters)==null?void 0:j.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Image'
+}} src={image} />`,...(x=(C=m.parameters)==null?void 0:C.docs)==null?void 0:x.source}}};var y,L,_;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:"() => <LazyImage />",...(_=(L=s.parameters)==null?void 0:L.docs)==null?void 0:_.source}}};var f,w,S;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Test Image'
+}} src={image}>
+    <Button color='green' content='Edit photo' icon='edit' onClick={action('edit')} />
+    <Button color='orange' content='Change photo' icon='move' onClick={action('change')} />
+    <Button color='red' content='Delete photo' icon='trash' onClick={action('delete')} />
+  </LazyImage>`,...(S=(w=i.parameters)==null?void 0:w.docs)==null?void 0:S.source}}};var E,z,v;n.parameters={...n.parameters,docs:{...(E=n.parameters)==null?void 0:E.docs,source:{originalSource:`() => <Card.Group>
+    <Card>
+      <LazyImage image={{
+      alt: 'Test Image'
+    }} src={image} />
+      <Card.Content header='Test 1' />
+    </Card>
+    <Card>
+      <LazyImage image={{
+      alt: 'Portrait Image'
+    }} src={portraitImage} />
+      <Card.Content header='Test 2' />
+    </Card>
+    <Card>
+      <LazyImage image={{
+      alt: 'Last Image'
+    }} />
+      <Card.Content header='Test 3' />
+    </Card>
+  </Card.Group>`,...(v=(z=n.parameters)==null?void 0:z.docs)==null?void 0:v.source}}};var P,T,N;c.parameters={...c.parameters,docs:{...(P=c.parameters)==null?void 0:P.docs,source:{originalSource:`() => <Item.Group>
+    <Item>
+      <Item.Image>
+        <LazyImage image={{
+        alt: 'First Image'
+      }} src={image} />
+      </Item.Image>
+      <Item.Content header='Test 1' />
+    </Item>
+    <Item>
+      <Item.Image>
+        <LazyImage image={{
+        alt: 'Portrait Image'
+      }} src={portraitImage} />
+      </Item.Image>
+      <Item.Content image={{
+      alt: 'Last Image'
+    }} header='Test 2' />
+    </Item>
+    <Item>
+      <Item.Image>
+        <LazyImage />
+      </Item.Image>
+      <Item.Content header='Test 3' />
+    </Item>
+  </Item.Group>`,...(N=(T=c.parameters)==null?void 0:T.docs)==null?void 0:N.source}}};var B,D,k;p.parameters={...p.parameters,docs:{...(B=p.parameters)==null?void 0:B.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Image'
+}} src='path/to/image.jpg' />`,...(k=(D=p.parameters)==null?void 0:D.docs)==null?void 0:k.source}}};var G,b,A;g.parameters={...g.parameters,docs:{...(G=g.parameters)==null?void 0:G.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Image'
+}} preview={image} src='path/to/image.jpg' />`,...(A=(b=g.parameters)==null?void 0:b.docs)==null?void 0:A.source}}};var U,W,F;d.parameters={...d.parameters,docs:{...(U=d.parameters)==null?void 0:U.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Image'
+}} preview='path/to/image.jpg' src={image} />`,...(F=(W=d.parameters)==null?void 0:W.docs)==null?void 0:F.source}}};var O,R,q;l.parameters={...l.parameters,docs:{...(O=l.parameters)==null?void 0:O.docs,source:{originalSource:`() => <LazyImage image={{
+  alt: 'Image'
+}} preview='path/to/image.jpg' src='path/to/image.jpg' />`,...(q=(R=l.parameters)==null?void 0:R.docs)==null?void 0:q.source}}};var H,J,K;I.parameters={...I.parameters,docs:{...(H=I.parameters)==null?void 0:H.docs,source:{originalSource:`() => <LazyImage downloadUrl={image} name='Image.jpg' image={{
+  alt: 'Image'
+}} src={image} />`,...(K=(J=I.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};const Ze=["Default","Placeholder","ExtraButtons","CardContent","ListContent","ErrorSource","ErrorSourceWithPreview","ErrorPreview","ErrorPreviewAndSource","Downloadable"];export{n as CardContent,m as Default,I as Downloadable,d as ErrorPreview,l as ErrorPreviewAndSource,p as ErrorSource,g as ErrorSourceWithPreview,i as ExtraButtons,c as ListContent,s as Placeholder,Ze as __namedExportsOrder,Ye as default};
