63306a569da149690d1adcd675114a6a2ce265d2
dleadbetter <derek@performantsoftware.com>
2025-06-25 09:48:54 -0400

    CDP #220 - Adding wrapper div with "relative" class to Timeline component

--- packages/core-data/src/components/Timeline.js
+++ packages/core-data/src/components/Timeline.js
@@ -339,160 +339,164 @@ const Timeline = (props: Props) => {
 
   return (
     <div
-      className={clsx(
-        'grow',
-        'px-4',
-        'pt-6',
-        'pb-2',
-        'flex',
-        'flex-col',
-        'justify-end',
-        'overflow-x-scroll',
-        'select-none',
-        dragging ? 'cursor-grabbing' : 'cursor-grab',
-      )}
-      ref={mergeRefs([frameRef, draggableRef])}
-      onPointerMove={onDrag}
-      onPointerUp={onDragEnd}
-      style={{ scrollbarWidth: 'none' }}
+      className='relative'
     >
-      <div className='absolute top-5 right-5 z-30 flex flex-row'>
-        <ButtonGroup
-          className='h-12'
-          rounded
-        >
-          <Button
-            aria-label={i18n.t('Timeline.zoomIn')}
-            className={clsx('w-12', 'pl-3', props.classNames?.zoom)}
-            disabled={!canZoomIn}
-            onClick={onZoomIn}
-            primary
-          >
-            <Icon name='magnifying_glass_plus' size={19} />
-          </Button>
-          <Button
-            aria-label={i18n.t('Timeline.zoomOut')}
-            className={clsx('w-12', 'pl-3', props.classNames?.zoom)}
-            disabled={!canZoomOut}
-            onClick={onZoomOut}
-            primary
-          >
-            <Icon name='magnifying_glass_minus' size={19} />
-          </Button>
-        </ButtonGroup>
-      </div>
       <div
-        className='h-full min-h-36 flex flex-col'
-        style={{ width: zoomWidth }}
-        onPointerDown={onDragStart}
+        className={clsx(
+          'grow',
+          'px-4',
+          'pt-6',
+          'pb-2',
+          'flex',
+          'flex-col',
+          'justify-end',
+          'overflow-x-scroll',
+          'select-none',
+          dragging ? 'cursor-grabbing' : 'cursor-grab',
+        )}
+        ref={mergeRefs([frameRef, draggableRef])}
+        onPointerMove={onDrag}
+        onPointerUp={onDragEnd}
+        style={{ scrollbarWidth: 'none' }}
       >
-        <div className='grow relative'>
-          {_.map(timelineEvents, (event) => (
-            <div
-              className='w-44 absolute'
-              key={event.uuid}
-              style={{
-                left: `${event.xOffset}px`,
-                bottom: `${event.yOffset + TIMELINE_PADTOP}px`,
-              }}
+        <div className='absolute top-5 right-5 z-30 flex flex-row'>
+          <ButtonGroup
+            className='h-12'
+            rounded
+          >
+            <Button
+              aria-label={i18n.t('Timeline.zoomIn')}
+              className={clsx('w-12', 'pl-3', props.classNames?.zoom)}
+              disabled={!canZoomIn}
+              onClick={onZoomIn}
+              primary
             >
-              <svg
-                className={clsx(
-                  'absolute',
-                )}
-                height={event.yOffset + TIMELINE_PADTOP + MARKER_DIAMETER}
-                width={MARKER_DIAMETER}
+              <Icon name='magnifying_glass_plus' size={19} />
+            </Button>
+            <Button
+              aria-label={i18n.t('Timeline.zoomOut')}
+              className={clsx('w-12', 'pl-3', props.classNames?.zoom)}
+              disabled={!canZoomOut}
+              onClick={onZoomOut}
+              primary
+            >
+              <Icon name='magnifying_glass_minus' size={19} />
+            </Button>
+          </ButtonGroup>
+        </div>
+        <div
+          className='h-full min-h-36 flex flex-col'
+          style={{ width: zoomWidth }}
+          onPointerDown={onDragStart}
+        >
+          <div className='grow relative'>
+            {_.map(timelineEvents, (event) => (
+              <div
+                className='w-44 absolute'
+                key={event.uuid}
                 style={{
-                  left: `${event.anchorRight
-                    ? EVENT_WIDTH + EVENT_LEFT_BORDER - MARKER_RADIUS
-                    : -MARKER_RADIUS}px`,
-                  top: MARKER_LINE_OFFSET
+                  left: `${event.xOffset}px`,
+                  bottom: `${event.yOffset + TIMELINE_PADTOP}px`,
                 }}
               >
-                <line
-                  x1={MARKER_RADIUS}
-                  x2={MARKER_RADIUS}
-                  y1={0}
-                  y2={event.yOffset + TIMELINE_PADTOP + MARKER_RADIUS - TIMELINE_HEIGHT}
-                  className='stroke-neutral-300 stroke-[1px]'
-                  shapeRendering='crispEdges'
-                />
-                <circle
-                  cx={MARKER_RADIUS}
-                  cy={event.yOffset + TIMELINE_PADTOP + MARKER_RADIUS - TIMELINE_HEIGHT}
-                  r={MARKER_RADIUS}
-                  className={clsx(props.classNames?.marker || 'fill-neutral-500')}
-                />
-              </svg>
-              <button
-                className={clsx(
-                  'h-9',
-                  'w-44',
-                  'px-2',
-                  'flex',
-                  'items-center',
-                  'bg-neutral-100',
-                  'active:bg-neutral-300',
-                  'outline',
-                  'outline-1',
-                  'outline-neutral-300',
-                  'rounded-tl',
-                  'rounded-tr',
-                  'relative',
-                  'z-10',
-                  'hover:z-20',
-                  'hover:bg-neutral-200',
-                  'text-neutral-800',
-                  event.anchorRight ? 'rounded-bl' : 'rounded-br',
-                  'mb-1',
-                )}
-                onPointerDown={(e) => e.stopPropagation()}
-                onClick={() => props.onClick(event)}
-                type='button'
+                <svg
+                  className={clsx(
+                    'absolute',
+                  )}
+                  height={event.yOffset + TIMELINE_PADTOP + MARKER_DIAMETER}
+                  width={MARKER_DIAMETER}
+                  style={{
+                    left: `${event.anchorRight
+                      ? EVENT_WIDTH + EVENT_LEFT_BORDER - MARKER_RADIUS
+                      : -MARKER_RADIUS}px`,
+                    top: MARKER_LINE_OFFSET
+                  }}
+                >
+                  <line
+                    x1={MARKER_RADIUS}
+                    x2={MARKER_RADIUS}
+                    y1={0}
+                    y2={event.yOffset + TIMELINE_PADTOP + MARKER_RADIUS - TIMELINE_HEIGHT}
+                    className='stroke-neutral-300 stroke-[1px]'
+                    shapeRendering='crispEdges'
+                  />
+                  <circle
+                    cx={MARKER_RADIUS}
+                    cy={event.yOffset + TIMELINE_PADTOP + MARKER_RADIUS - TIMELINE_HEIGHT}
+                    r={MARKER_RADIUS}
+                    className={clsx(props.classNames?.marker || 'fill-neutral-500')}
+                  />
+                </svg>
+                <button
+                  className={clsx(
+                    'h-9',
+                    'w-44',
+                    'px-2',
+                    'flex',
+                    'items-center',
+                    'bg-neutral-100',
+                    'active:bg-neutral-300',
+                    'outline',
+                    'outline-1',
+                    'outline-neutral-300',
+                    'rounded-tl',
+                    'rounded-tr',
+                    'relative',
+                    'z-10',
+                    'hover:z-20',
+                    'hover:bg-neutral-200',
+                    'text-neutral-800',
+                    event.anchorRight ? 'rounded-bl' : 'rounded-br',
+                    'mb-1',
+                  )}
+                  onPointerDown={(e) => e.stopPropagation()}
+                  onClick={() => props.onClick(event)}
+                  type='button'
+                >
+                  <span className='truncate text-sm'>
+                    {event.name}
+                  </span>
+                </button>
+              </div>
+            ))}
+          </div>
+          <div ref={ticksRef} className='w-fill'>
+            {ticks && (
+              <svg
+                className='!overflow-visible w-fill'
+                height={40}
+                width='100%'
+                overflow='visible'
+                shapeRendering='crispEdges'
               >
-                <span className='truncate text-sm'>
-                  {event.name}
-                </span>
-              </button>
-            </div>
-          ))}
-        </div>
-        <div ref={ticksRef} className='w-fill'>
-          {ticks && (
-            <svg
-              className='!overflow-visible w-fill'
-              height={40}
-              width='100%'
-              overflow='visible'
-              shapeRendering='crispEdges'
-            >
-              {/* Baseline */}
-              {_.last(ticks.major) && (
-                <line x1='0' y1='0' x2={_.last(ticks.major).xOffset + SVG_WIGGLE_ROOM} y2='0' stroke='currentColor' />
-              )}
-              {/* Ticks and labels */}
-              {_.map(ticks.major, ({ value, xOffset, hideVal }, i) => !hideVal && (
-                <g key={value} transform={`translate(${xOffset}, 0)`}>
-                  <line y2={10} stroke='currentColor' />
-                  <text
-                    className='translate-y-8'
-                    key={value}
-                    style={{
-                      textAnchor: 'middle',
-                    }}
-                    fill='currentColor'
-                  >
-                    {displayTickDate(value, i !== 0 ? ticks.major[i - 1].value : null)}
-                  </text>
-                </g>
-              ))}
-              {_.map(ticks.minor, ({ xOffset }, i) => (
-                <g key={i} transform={`translate(${xOffset}, 0)`}>
-                  <line y2={5} stroke='currentColor' />
-                </g>
-              ))}
-            </svg>
-          )}
+                {/* Baseline */}
+                {_.last(ticks.major) && (
+                  <line x1='0' y1='0' x2={_.last(ticks.major).xOffset + SVG_WIGGLE_ROOM} y2='0' stroke='currentColor' />
+                )}
+                {/* Ticks and labels */}
+                {_.map(ticks.major, ({ value, xOffset, hideVal }, i) => !hideVal && (
+                  <g key={value} transform={`translate(${xOffset}, 0)`}>
+                    <line y2={10} stroke='currentColor' />
+                    <text
+                      className='translate-y-8'
+                      key={value}
+                      style={{
+                        textAnchor: 'middle',
+                      }}
+                      fill='currentColor'
+                    >
+                      {displayTickDate(value, i !== 0 ? ticks.major[i - 1].value : null)}
+                    </text>
+                  </g>
+                ))}
+                {_.map(ticks.minor, ({ xOffset }, i) => (
+                  <g key={i} transform={`translate(${xOffset}, 0)`}>
+                    <line y2={5} stroke='currentColor' />
+                  </g>
+                ))}
+              </svg>
+            )}
+          </div>
         </div>
       </div>
     </div>
