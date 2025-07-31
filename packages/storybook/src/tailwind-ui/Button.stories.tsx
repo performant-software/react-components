1de02503cb3b302f5d9837aee79f7a7136d151f9
Camden <c@camdenmecklem.com>
2025-07-31 14:20:13 -0400

    Tailwind `Button` component (#534)
    * button component
    
    feedback
    
    * linting
    
    * consolidate colors

---
+++ packages/storybook/src/tailwind-ui/Button.stories.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import Button from '../../../tailwind-ui/src/components/Button';
+import { FaPlus } from "react-icons/fa";
+import DarkContainer from './DarkContainer';
+
+export default {
+  title: 'Components/TailwindUI/Button',
+  component: Button
+};
+
+export const Default = () => {
+  return (
+    <div className='flex flex-wrap gap-8'>
+      <Button variant='filled'>
+        click me
+      </Button>
+      <Button variant='outline'>
+        click me
+      </Button>
+      <Button variant='plain'>
+        click me
+      </Button>
+    </div>
+  );
+};
+
+export const Dark = () => {
+  return (
+    <DarkContainer>
+      <div className='flex flex-wrap gap-8'>
+        <Button variant='filled'>
+          click me
+        </Button>
+        <Button variant='outline'>
+          click me
+        </Button>
+        <Button variant='plain'>
+          click me
+        </Button>
+      </div>
+    </DarkContainer>
+  );
+};
+
+export const Disabled = () => {
+  return (
+    <div className='flex flex-wrap gap-8'>
+      <Button disabled variant='filled'>
+        don&apos;t click me
+      </Button>
+      <Button disabled variant='outline'>
+        don&apos;t click me
+      </Button>
+      <Button disabled variant='plain'>
+        don&apos;t click me
+      </Button>
+    </div>
+  );
+};
+
+export const DisabledDark = () => {
+  return (
+    <DarkContainer>
+      <div className='flex flex-wrap gap-8'>
+        <Button disabled variant='filled'>
+          don&apos;t click me
+        </Button>
+        <Button disabled variant='outline'>
+          don&apos;t click me
+        </Button>
+        <Button disabled variant='plain'>
+          don&apos;t click me
+        </Button>
+      </div>
+    </DarkContainer>
+  );
+};
+
+export const Icons = () => {
+  return (
+    <div className='flex flex-wrap gap-8'>
+      <Button iconOnly variant='filled'>
+        <FaPlus />
+      </Button>
+      <Button iconOnly variant='outline'>
+        <FaPlus />
+      </Button>
+      <Button iconOnly variant='plain'>
+        <FaPlus />
+      </Button>
+    </div>
+  );
+};
+
+export const IconsDark = () => (
+  <DarkContainer>
+    <div className='flex flex-wrap gap-8'>
+      <Button iconOnly variant='filled'>
+        <FaPlus />
+      </Button>
+      <Button iconOnly variant='outline'>
+        <FaPlus />
+      </Button>
+      <Button iconOnly variant='plain'>
+        <FaPlus />
+      </Button>
+    </div>
+  </DarkContainer>
+);
+
+export const CustomClass = () => (
+  <Button
+    className='bg-pink-500!'
+  >
+    click me
+  </Button>
+);
+
+export const AriaLabel = () => (
+  <Button
+    ariaLabel='click me'
+  >
+    click me
+  </Button>
+);
---
+++ packages/tailwind-ui/src/components/Button.tsx
@@ -0,0 +1,51 @@
+import clsx from 'clsx';
+import React, { useMemo } from 'react';
+
+interface Props {
+  ariaLabel?: string
+  className?: string
+  children?: React.ElementType | React.ElementType[]
+  disabled?: boolean
+  iconOnly?: boolean
+  onClick?: (...args: any[]) => any
+  size?: 'xs' | 's' | 'base' | 'l' | 'xl'
+  type?: 'button' | 'submit' | 'reset'
+  variant?: 'filled' | 'outline' | 'plain'
+}
+
+const Button: React.FC<Props> = (props) => {
+  const size = useMemo(() => props.size || 'base', [props.size]);
+  const variant = useMemo(() => props.variant || 'filled', [props.variant]);
+
+  return (
+    <button 
+      aria-label={props.ariaLabel}
+      className={clsx(
+        'font-semibold rounded-md outline-offset-2 focus:outline-2 focus:outline-primary disabled:opacity-50',
+        {
+          'bg-primary text-white hover:brightness-110': variant === 'filled',
+          'bg-white border border-zinc-200 hover:bg-zinc-200 dark:hover:bg-white/5 dark:border-zinc-700 dark:bg-zinc-900 dark:text-white dark:disabled:text-zinc-400': variant === 'outline',
+          'hover:bg-black/10 dark:text-white dark:hover:bg-transparent dark:disabled:text-zinc-400': variant === 'plain',
+          'py-1 px-1': size === 'xs' && props.iconOnly,
+          'py-1.5 px-1.5': size === 's' && props.iconOnly,
+          'py-2 px-2': size === 'base' && props.iconOnly,
+          'py-2.5 px-2.5': size === 'l' && props.iconOnly,
+          'py-3 px-3': size === 'xl' && props.iconOnly,
+          'py-1 px-2 text-xs': size === 'xs' && !props.iconOnly,
+          'py-1 px-2 text-sm': size === 's' && !props.iconOnly,
+          'py-1.5 px-2.5 text-sm': size === 'base' && !props.iconOnly,
+          'py-2 px-3 text-sm': size === 'l' && !props.iconOnly,
+          'py-2 px-4 text-base': size === 'xl' && !props.iconOnly
+        },
+        props.className
+      )}
+      disabled={props.disabled}
+      onClick={props.onClick}
+      type={props.type || 'button'}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
