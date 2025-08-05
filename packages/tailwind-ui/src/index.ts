08e9120d6ebbb87d61f9071029b74aad5f4df96f
Camden Mecklem <c@camdenmecklem.com>
2025-08-05 15:27:15 -0400

    add export

--- packages/tailwind-ui/src/index.ts
+++ packages/tailwind-ui/src/index.ts
@@ -4,3 +4,4 @@ export { default as Dropdown } from './components/Dropdown';
 export { default as Input } from './components/Input';
 export { default as Listbox } from './components/Listbox';
 export { default as Navbar } from './components/Navbar';
+export { default as Dialog } from './components/Dialog';
