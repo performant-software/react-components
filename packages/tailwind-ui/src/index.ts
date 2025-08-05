3bbc246db5413f0cdf2e9a79a4a8efd1a4a952db
Camden <c@camdenmecklem.com>
2025-08-05 10:12:37 -0400

    Tailwind `Listbox` component (#540)
    * WIP
    
    * custom styles

--- packages/tailwind-ui/src/index.ts
+++ packages/tailwind-ui/src/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './components/Button';
+export { default as Card } from './components/Card';
 export { default as Dropdown } from './components/Dropdown';
 export { default as Input } from './components/Input';
+export { default as Listbox } from './components/Listbox';
 export { default as Navbar } from './components/Navbar';
-export { default as Card } from './components/Card';
