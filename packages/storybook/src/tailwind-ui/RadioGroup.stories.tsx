ad0497049ead0ef0511edc2cc9474facab86334e
Camden <c@camdenmecklem.com>
2025-08-06 11:26:48 -0400

    Performant UI `RadioGroup` component (#541)
    * radio button
    
    * cleanup
    
    * cleanup

---
+++ packages/storybook/src/tailwind-ui/RadioGroup.stories.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import RadioGroup from '../../../tailwind-ui/src/components/RadioGroup';
+
+export default {
+  title: 'Components/TailwindUI/RadioGroup',
+  component: RadioGroup
+};
+
+const options = [
+  {
+    id: 1,
+    label: 'Admin'
+  },
+  {
+    id: 2,
+    label: 'Moderator'
+  },
+  {
+    id: 3,
+    label: 'User'
+  }
+];
+
+const optionWithDescriptions = [
+  {
+    id: 1,
+    label: 'Admin',
+    description: 'Admins control everything on the site!'
+  },
+  {
+    id: 2,
+    label: 'Moderator',
+    description: 'Moderators can wield the BAN HAMMER.'
+  },
+  {
+    id: 3,
+    label: 'User',
+    description: 'Just a regular user.'
+  }
+];
+
+export const Default = () => {
+  const [value, setValue] = useState(options[0]);
+
+  return (
+    <RadioGroup
+      onChange={setValue}
+      options={options}
+      value={value}
+    />
+  );
+};
+
+export const Descriptions = () => {
+  const [value, setValue] = useState(optionWithDescriptions[0]);
+
+  return (
+    <RadioGroup
+      onChange={setValue}
+      options={optionWithDescriptions}
+      value={value}
+    />
+  );
+};
+
+export const CustomStyles = () => {
+  const [value, setValue] = useState(optionWithDescriptions[0]);
+
+  return (
+    <RadioGroup
+      classes={{
+        root: 'text-red-200!',
+        field: 'gap-10!',
+        button: 'bg-purple-800!',
+        label: 'lowercase!',
+        description: 'uppercase!'
+      }}
+      onChange={setValue}
+      options={optionWithDescriptions}
+      value={value}
+    />
+  );
+};
