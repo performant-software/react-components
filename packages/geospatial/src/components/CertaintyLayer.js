f24200a43c58c27f51a23e25b81b1967cbf47a55
Camden Mecklem <c@camdenmecklem.com>
2026-04-09 14:33:37 -0400

    DRY

--- packages/geospatial/src/components/CertaintyLayer.js
+++ packages/geospatial/src/components/CertaintyLayer.js
@@ -1,11 +1,9 @@
 // @flow
 
-import circle from '@turf/circle';
-import { featureCollection } from '@turf/helpers';
-import React, { useCallback, useMemo } from 'react';
+import React from 'react';
 import { Layer, Source } from 'react-map-gl/maplibre';
-import _ from 'underscore';
 import MapStyles from '../utils/MapStyles';
+import MapUtils from '../utils/Map';
 
 type Props = {
   geometry?: any,
@@ -16,33 +14,7 @@ type Props = {
  * Renders circles with the given certainty_radius circumference around all points in a new layer.
  */
 const CertaintyLayer = (props: Props) => {
-  const buildCircle = useCallback((point) => (
-    circle(point.coordinates, props.certaintyRadius, { units: 'kilometers', steps: 32 })
-  ), [props.certaintyRadius]);
-
-  const circles = useMemo(() => {
-    const features = [];
-
-    if (props.certaintyRadius > 0 && props.geometry) {
-      if (props.geometry.type === 'FeatureCollection') {
-        _.each(props.geometry.features, (feature) => {
-          if (feature.geometry?.type === 'Point') {
-            features.push(buildCircle(feature.geometry));
-          }
-        });
-      } else if (props.geometry.type === 'GeometryCollection') {
-        _.each(props.geometry.geometries, (geometry) => {
-          if (geometry.type === 'Point') {
-            features.push(buildCircle(geometry));
-          }
-        });
-      } else if (props.geometry?.type === 'Point') {
-        features.push(buildCircle(props.geometry));
-      }
-    }
-
-    return featureCollection(features);
-  }, [props.geometry, props.certaintyRadius]);
+  const circles = MapUtils.getCertaintyCircles([props.geometry], () => props.certaintyRadius);
 
   return (
     <Source
